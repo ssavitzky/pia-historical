e4bbe5e01d3ba12d5b88088100d8cd70d3eab0ee
steve <>
1996-11-14 23:09:01 +0000

    Figured it out -- has to be setuid root

--- src/app/setuid/run_pia.c
+++ src/app/setuid/run_pia.c
@@ -1,9 +1,20 @@
+/**** run_pia
+** 	$Id$
+**
+**	Run ~pia/bin/pia as user pia.  Must be setuid ROOT.
+**	Install with: chown root run_pia; chmod u+s run_pia
+**
+**	If installed owned by user "pia", perl somehow knows that it's
+**	running setuid, and complains.  Deep magic.
+*/
+
 #include <stdlib.h>
 #include <sys/types.h>
 #include <pwd.h>
+#include <stdio.h>
 
 char *args[] = {
-  "/usr/local/bin/perl", "-U",
+  "/usr/local/bin/perl",/* "-U", */
   "/home/pia/bin/pia",
   0
 };
@@ -14,11 +25,11 @@ main(int argc, char** argv, char** env)
   struct passwd *pw = getpwnam("pia");
 
   setenv("USER", pw->pw_name, 1);
-  setenv("LOGNAME", pw->pw_name, 1);	/* doesn't work with /bin/bash */
+  setenv("LOGNAME", pw->pw_name, 1);	
   setenv("HOME", pw->pw_dir, 1);
 
   if (setuid(pw->pw_uid)) {
-    printf("sorry\n");
+    fprintf(stderr, "sorry\n");
     exit(-1);
   }
   setgid(pw->pw_gid);	
