c42529d29607bd40d20a44fc1ed80cf5c443db0d
rithy <>
1997-02-20 23:24:39 +0000

    initial version

---
+++ src/java/crc/pia/agent/AgntMach.java
@@ -0,0 +1,81 @@
+// AgntMach.java
+// $Id$
+// (c) COPYRIGHT Ricoh California Research Center, 1997.
+
+/**
+ * subclass of machine for agents, these are really virtual machines
+ * used by agents when they want to receive transactions
+ */
+
+package crc.pia.agent;
+import crc.pia.Machine;
+import crc.pia.Agent; 
+
+class AgntMach extends Machine {
+  /**
+   * Agent that creates this machine
+   */
+  protected Agent agent;
+
+  /**
+   * Callback functor
+   */
+  protected  xxx callback; 
+
+  public AgntMach( Agent agent ){
+    setAgent( agent );
+  }
+
+  /**
+   * set agent
+   */
+  public void setAgent( Agent agent ){
+    if( agent )
+      this.agent = agent;
+  }
+
+  /**
+   * @return agent
+   */
+  public Agent getAgent(){
+      return agent;
+  }
+
+  /**
+   * set callback functor
+   */
+  public void setCallback( xxx callback ){
+    if( callback ) this.callback = callback;
+  }
+
+  /**
+   * @return callback
+   */
+  public xxx getCallback(){
+      return callback;
+  }
+
+  /**
+   * send response using a predefined callback
+   */
+   public void sendResponse (Transaction reply, Resolver resolver) {
+     xxx cb = getCallback();
+     cb.execute(agent, reply, resolver);
+   }
+  
+  /**
+   * Handle a direct request to an agent.
+   * Normally done by running an InterForm, but the agent can 
+   * perform special processing first.
+   */
+  public Transaction getRequest(Transaction request, Resolver resolver) {
+    Transaction response = null;
+
+    Agent agnt = agent;
+    if( agnt )
+      response = agnt.respond(request, resolver);
+    return response;
+}
+
+
+
