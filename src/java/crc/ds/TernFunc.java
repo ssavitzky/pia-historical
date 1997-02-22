35773c5bf84be08e0333b1c3ff6207d222e319be
rithy <>
1997-02-22 00:24:14 +0000

    initial version

---
+++ src/java/crc/ds/TernFunc.java
@@ -0,0 +1,14 @@
+// TernFunc.java
+// $Id$
+// (c) COPYRIGHT Ricoh California Research Center, 1997.
+package crc.ds;
+
+public interface TernFunc
+  {
+  /**
+   * Return the result of executing with a three Object.
+   * @param object The object to process.
+   * @return The result of processing the input Object.
+   */
+  Object execute( Object object, Object object, Object object );
+  }
