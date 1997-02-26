3889cd99b3908bdedd4ce73c8acdb7f327de11c7
rithy <>
1997-02-26 01:32:50 +0000

    prev. IsFileReq.java

---
+++ src/java/crc/tf/IsFileRequest.java
@@ -0,0 +1,45 @@
+// IsFileRequest.java
+// $Id$
+// (c) COPYRIGHT Ricoh California Research Center, 1997.
+
+ 
+  /**
+   *Feature Computers.
+   *All take a transaction as their argument, and most return a
+   *boolean.  Feature computers may use the utility method
+   *transaction->assert(name,value) to set additional features. 
+   *
+   *By convention, a feature computer "is_foo" computes a feature
+   *named "foo". 
+   *
+   *Default Features: 
+   *	These are computed by default when a transaction is created;
+   *	they may have to be recomputed if the transaction is modified.
+   *
+   */
+package crc.tf;
+
+import crc.ds.UnaryFunctor;
+
+public final class IsFileRequest implements UnaryFunctor{
+
+  /**
+   * 
+   * @param object A transaction 
+   * @return object boolean
+   */
+    public Object execute( Object trans ){
+      String scheme = trans.getProtocol();
+      if( scheme ){
+	String lscheme = scheme.toLowerCase();
+	if( lscheme.indexOf("file") != -1 )
+	  return new Boolean( true );
+      }
+      return new Boolean( false );
+    }
+}
+
+
+
+
+
