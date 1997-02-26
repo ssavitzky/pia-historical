55c4132023bd33f976a82e0b5cadc3586589af72
rithy <>
1997-02-26 01:43:57 +0000

    new name

---
+++ src/java/crc/tf/IsRequest.java
@@ -0,0 +1,35 @@
+// IsRequest.java
+// $Id$
+// (c) COPYRIGHT Ricoh California Research Center, 1997.
+
+ 
+  /**
+   *Feature Computers.
+   *All take a transaction as their argument, and most return a
+   *boolean.  Feature computers may use the utility method
+   *transaction->assert(name,value) to set additional features. 
+   *
+   *By convention, a feature computer "is_foo" computes a feature
+   *named "foo". 
+   *
+   *Default Features: 
+   *	These are computed by default when a transaction is created;
+   *	they may have to be recomputed if the transaction is modified.
+   *
+   */
+package crc.tf;
+
+import crc.ds.UnaryFunctor;
+
+public final class IsRequest implements UnaryFunctor{
+
+  /**
+   * 
+   * @param object A transaction 
+   * @return object boolean
+   */
+    public Object execute( Object trans ){
+      return new Boolean( trans.isRequest() );
+    }
+}
+
