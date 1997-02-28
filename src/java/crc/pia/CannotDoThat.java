04d06869ba1a37a21afeb822fb11487e5c9c1c1c
rithy <>
1997-02-28 00:31:26 +0000

    initial version

---
+++ src/java/crc/pia/CannotDoThat.java
@@ -0,0 +1,12 @@
+// CannotDoThat.java
+// $Id$
+// (c) COPYRIGHT Ricoh California Research Center, 1997.
+
+package crc.pia;
+
+public class CannotDoThat extends Exception {
+    
+    public CannotDoThat(String msg) {
+	super(msg);
+    }
+}
