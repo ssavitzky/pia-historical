db748bd8db496cc148e34b8928ccd2b050183c7a
rithy <>
1997-03-12 17:19:34 +0000

    create content body base on mime type.

--- src/java/crc/pia/ContentFactory.java
+++ src/java/crc/pia/ContentFactory.java
@@ -10,42 +10,48 @@ import java.io.InputStream;
  */
 public class ContentFactory
 {
-/**  creates a new content factory with default list of possible content objects
+  /**
+   * first line of stream
+   */
+  protected StringBuffer buf;
+
+ /**  creates a new content factory with default list of possible content objects
  */
   public ContentFactory()
   {
   }
-  
-/** create a content object from the given stream
- */
-  public Content createContent(InputStream http)
+
+
+  /** create a content object from the given stream
+   */
+  public Content createContent(String contentType, InputStream input)
   {
-    //we need a better data structure that hash table for headers
-    //parse the entire header, then check for POST or GET method
-    //If POST, create a ParamPropList kind (may passing content in constructor)
-    //If GET, do the same as POST passing querystring from url if there is a query string
-    
-    Hashtable headers=parseHeaders(http);
-    
-    Content object=contentFor(headers);
-    
-    object.headers(headers);
-    
-    object.source(http);
-    
-    return object;
-    
-  }
-  
-/** map header content type to proper object
- */
-  private Content contentFor(Hashtable hash)
-  {
-    String type=hash.get("content-type");
-    
-    //for now, just use byte stream
-    return new ByteStreamContent();
+    Content c = null;
+
+    try{
+      MimeType ztype  = new MimeType( contentType );
+      // This is too simple,minded.  Need to test others
+      if( ztype.match( APPLICATION_X_WWW_FORM_URLENCODED ) )
+	c = new FormContent();
+      else
+	c = new ByteStreamContent();
+
+      c.source( input );
+
+      return newContent;
+    }catch(Exception ex){
+      ex.printStackTrace();
+      System.out.println("MimeParser <factory> <file>");
+    }
     
   }
   
 }
+
+
+
+
+
+
+
+
