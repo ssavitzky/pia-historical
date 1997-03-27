7c3b2ec51ddee7ed41b6ba2f98aa9a5d578f7957
steve <>
1997-03-27 01:17:19 +0000

    Mostly complete; no major holes, but untested.

--- src/java/crc/interform/Parser.java
+++ src/java/crc/interform/Parser.java
@@ -39,11 +39,23 @@ import java.util.BitSet;
  *
  *	@see http:/PIA/src/lib/perl/IFParser.pm
  *
+ * Nomenclature: Methods with names starting in <code>eat</code>
+ *	attempt to consume a lexical token from the input stream, and
+ *	return <code>true</code> if they are successful.  If they
+ *	fail, they leave the input stream unchanged.  Methods with
+ *	names starting in <code>get</code> attempt to consume a more
+ *	complex construct, and leave their rejected input in
+ *	<code>buf</code> if they fail. <p>
+ *
  * NOTE: The Parser is currently unable to correctly handle SGML
  *	comments that contain the string "-->" internally.  This is
  *	the only construct that requires more than a single character
  *	of lookahead. <p>
- */
+ *
+ * NOTE: We are NOT using a StreamTokenizer at this point.  The reason
+ *	is that we need all characters to be significant and
+ *	``ordinary'' outside of tags, so the StreamTokenizer would
+ *	simply add an extra layer of overhead for very little effect. <p> */
 class Parser extends Input {
 
   /************************************************************************
@@ -65,7 +77,7 @@ class Parser extends Input {
    *	the <em>next</em> token if non-null. */
   int last;
 
-  /** Holds the next item, almost invariably an entity reference. */
+  /** Holds the next item, usually either an entity reference or a tag. */
   SGML next;
 
   /** If true, nextInput will wait for more input.  If false, it will
@@ -75,11 +87,6 @@ class Parser extends Input {
 
   /** If true, there is no more input. */
   public boolean done = false;
-
-  /* === We are NOT using a StreamTokenizer at this point.  The reason
-   *	is that we need all characters to be significant and
-   *	``ordinary'' outside of tags, so the StreamTokenizer would
-   *	simply add an extra layer of overhead for very little effect.  */
 
   /************************************************************************
   ** Input (pull) Interface:
@@ -148,11 +155,24 @@ class Parser extends Input {
   ** Syntax tables:
   ************************************************************************/
 
+  /** True for every character that is part of an identifier.  Does not
+   *	distinguish the characters ('-' and '.') that are not officially
+   *	permitted at the <em>beginning</em> of an identifier. */
   static BitSet isIdent;
+
+  /** True for every character that is whitespace. */
   static BitSet isSpace;
   
-  void initializeTables() {
-
+  /** Initialize the identifier and whitespace BitSet's.  Since we are only 
+   *	concerned with the SGML reference syntax, we don't have to make these 
+   *	public or have a set for each Parser object. */
+  static void initializeTables() {
+    for (int i = 0; i <= ' '; ++i) isSpace.set(i);
+    for (int i = 'A'; i <= 'Z'; ++i) isIdent.set(i);
+    for (int i = 'a'; i <= 'z'; ++i) isIdent.set(i);
+    for (int i = '0'; i <= '9'; ++i) isIdent.set(i);
+    isIdent.set('-');
+    isIdent.set('.');
   }
 
   /************************************************************************
@@ -201,14 +221,16 @@ class Parser extends Input {
    *	quote) is seen.
    *
    *	@return false if end-of-file is reached before a match. */
-  boolean eatUntil(int aCharacter) throws IOException {
+  boolean eatUntil(int aCharacter, boolean checkEntities) throws IOException {
     last = in.read();
     if (last < 0) return false;
+    if (checkEntities && last == '&') return false;
     if (last == aCharacter) return true;
     do {
       buf.append((char)last);
       last = in.read();
-    } while (last >= 0 && last != aCharacter);
+    } while (last >= 0 && last != aCharacter
+	     && !(checkEntities && last == '&'));
     return last >= 0;    
   }
 
@@ -217,10 +239,26 @@ class Parser extends Input {
    *	tag) is matched.
    *
    *	@return false if end-of-file is reached before a match. */
-  boolean eatUntil(String aString) throws IOException {
+  boolean eatUntil(String aString, boolean checkEntities) throws IOException {
 
     return false;
   }
+
+  /** Starting at the next available character, append spaces to 
+   *	<code>buf</code> until a non-blank character is reached.
+   */
+  boolean eatSpaces() throws IOException {
+    last = in.read();
+    if (last < 0) return false;
+    if (last <= ' ') return true;
+    do {
+      buf.append((char)last);
+      last = in.read();
+    } while (last >= 0 && last <= ' ');
+    return last >= 0;    
+  }
+
+
 
   /************************************************************************
   ** SGML Recognizers:
@@ -229,7 +267,7 @@ class Parser extends Input {
   /** Pull an entity off the input stream and return it in <code>next</code>.
    *	Assume that <code>last</code> contains an ampersand.  If the next
    *	available character does not belong in an identifier, appends the
-   *	ampersand to <code>buf</code>.
+   *	ampersand to <code>buf</code>.  Eat a trailing semicolon if present.
    *	@return false if the next available character does not belong in
    *	an entity name.
    */
@@ -239,30 +277,72 @@ class Parser extends Input {
       buf.append((char)last);
       return false;
     }
-    next = new Token("&", ident, ";");
+    next = new Token("&", ident, (last == ';')? ";" : null);
     if (last == ';') last = 0;
     return true;
   }
 
-  /** Scan <code>aBuffer</code> (typically <code>buf</code>) for 
-   *	entity references and return either a Text or Tokens object.
-   *
-   *	@return false if the next available character does not belong in
-   *	an identifier. */
-  SGML scanForEntities(StringBuffer aBuffer) {
-
-    return null;
-  }
-
   /** Get a literal, i.e. everything up to <code>endString</code>.
-   *	Clear endString and ignoreMarkup when the end string is seen.
-   *	If ignoreMarkup is false, entities will be recognized.
+   *	Clear endString and ignoreEntities when the end string is seen.
+   *	If ignoreEntities is false, entities will be recognized.
    */
   SGML getLiteral() {
 
-    // === 
+    buf = new StringBuffer();
+    Tokens list = new Tokens();
+    
+    try {
+      for ( ; ; ) {
+	if (eatUntil(endString, !ignoreEntities)) {
+	  if (list.isEmpty() || ! (buf.length() == 0)) 
+	    list.append(new Text(buf));
+	  break;
+	}
+	if (getEntity()) {
+	  list.append(next);
+	}
+      }
+    } catch (Exception e) {}
+    endString = null;
+    ignoreEntities = false;
+    return list;
+  }
 
-    return null;
+  /** Get a value (after an attribute name inside a tag.
+   *	Returns true if <code>last</code> is an equal sign and is 
+   *	followed by either an identifier or a quoted string.
+   *	@return the value in <code>next</code>
+   */
+  boolean getValue() throws IOException {
+    if (last != '=') return false;
+
+    last = 0;
+    if (eatIdent()) {
+      next = new Text(ident);
+      return true;
+    } else if (last == '\'' || last == '"') {
+      int quote = last;
+      StringBuffer tmp = buf;
+      buf = new StringBuffer();
+      Tokens list = new Tokens();
+      
+      for ( ; ; ) {
+	if (eatUntil(quote, true)) {
+	  if (list.isEmpty() || ! (buf.length() == 0)) 
+	    list.append(new Text(buf));
+	  break;
+	}
+	if (getEntity()) {
+	  list.append(next);
+	}
+      }
+      next = list.isText()? (SGML)list.toText() : (SGML)list;
+      buf = tmp;
+      return true;
+    } else {
+      // === maybe should try nonblanks
+      return false;
+    }
   }
 
   /** Get a tag starting with <code>last</code> and return it in
@@ -271,20 +351,56 @@ class Parser extends Input {
    *	to <code>buf</code>.  getTag is only called from getText. <p>
    *
    *	One could argue that it should allow space after the &lt;. */
-  boolean getTag() {
+  boolean getTag() throws IOException {
 
     if (eatIdent()) {		// <tag...	start tag
+      // Save our position in buf in case we lose.
+      int tagStart = buf.length();
 
-      // === for now assume we have a start tag
+      Token it = new Token(ident);
+      String a; StringBuffer v;
+
+      // Now go after the attributes.
+      //    They have to be separated by spaces.
+
+      while (last >= ' ') {
+	// === need to be appending the identifier in case we lose ===
+	eatSpaces();	
+	if (eatIdent()) {
+	  a = ident;
+	  if (getValue()) it.attr(a, next);
+	  else		  it.attr(a, new Tokens());
+	}
+      }
+
+      // Done.  Clean up the buffer and return the new tag in next.
+      buf.setLength(tagStart);
+      next = it;
+      if (last >= 0) last = 0;
     } else if (last == '/') {	// </...	end tag
       eatIdent();
       next = new Token(ident);
-      next.incomplete = -1;
+      next.incomplete((byte)-1);
     } else if (last == '!') {	// <!...	comment or declaration
-
+      StringBuffer tmp = buf;
+      buf = new StringBuffer();
+      eatIdent();		// note that -- is an SGML identifier
+      if (ident.length() >= 2 && // ... so check for it.
+	  ident.charAt(0) == '-' && ident.charAt(1) == '-') {
+	// it must be a comment
+	if (last != '>') eatUntil("-->", false);
+	if (last == '>') last = 0;
+	next = new Token("!--", "<!" + ident, buf, "-->");
+      } else {
+	// it's an SGML declaration: <!...>
+	eatUntil('>', false);
+	if (last == '>') last = 0;
+	next = new Token("!", "<!" + ident, buf, ">");
+      }
+      buf = tmp;
     } else if (last == '>') {	// <>		empty start tag
       next = new Token(ident);
-      next.incomplete = 1;
+      next.incomplete((byte)1);
     } else {			// not a tag.
       return false;
     }
@@ -295,7 +411,7 @@ class Parser extends Input {
    *	terminated by an entity or tag, the entity or tag ends up in
    *	<code>next</code>, and the character that terminated
    *	<em>it</em> is left in <code>last</code>.  */
-  SGML getText() {
+  SGML getText() throws IOException {
 
     while (eatText()) {
       if ((last == '&' && getEntity()) ||
@@ -319,176 +435,16 @@ class Parser extends Input {
    *	state in order to do it.*/
   SGML getToken() {
     buf = new StringBuffer(256);
+    SGML it = null;
 
-    SGML it = getText();	// Try to get some text.
+    try {
+      it = getText();		// Try to get some text.
+    } catch (IOException e) {};
     if (it == null) {		// If that failed,
       it = next;		// 	a tag or entity must be in next
       next = null;		// 	so clear next for next time.
     }
-
     return it;
   }
 
-/* ========================================================================
-
-### The PERL version can be found in /PIA/src/lib/perl/IF/Parser.pm
-
-sub parse {
-    my $self = shift;
-
-    ## parse ($text)
-    ##	  Append $text to the buffer (which contains whatever couldn't 
-    ##	  be parsed from the previous chunk) and scan off as many tokens
-    ##	  as possible.
-
-    my $interp = exists $self->{'_interp'}? $self->interp : $self;
-    my $buf = \ $self->{'_buf'};
-    unless (defined $_[0]) {
-	# signals EOF (assume rest is plain text)
-	$interp->resolve($$buf) if length $$buf;
-	$$buf = '';
-	return $self;
-    }
-    $$buf .= $_[0];
-
-    # Parse html text in $$buf.  The strategy is to remove complete
-    # tokens from the beginning of $$buf until we can't deside whether
-    # it is a token or not, or the $$buf is empty.
-    while (1) {  # the loop will end by returning when text is parsed
-	# First we try to pull off any plain text (anything before a "<" char)
-	if ($$buf =~ s|^([^<]+)||) {
-	    unless (length $$buf) {
-		my $text = $1;
-		# At the end of the buffer, we should not parse white space
-		# but leave it for parsing on the next round.
-		if ($text =~ s|(\s+)$||) {
-		    $$buf = $1;
-                # Same treatment for chopped up entites.
-		} elsif ($text =~ s/(&(?:(?:\#\d*)?|[-.\w]*))$//) {
-		    $$buf = $1;
-		};
-		$interp->resolve($text);
-		return $self;
-	    } else {
-		$interp->resolve($1);
-	    }
-	# Then, markup declarations (usually either <!DOCTYPE...> or a comment)
-	} elsif ($$buf =~ s|^(<!)||) {
-	    my $eaten = $1;
-	    my $text = '';
-	    # Eat text and beginning of comment
-	    while ($$buf =~ s|^(([^>]*?)--)||) {
-		$eaten .= $1;
-		$text .= $2;
-		# Look for end of comment
-		if ($$buf =~ s|^((.*?)--)||s) {
-		    $eaten .= $1;
-		    $self->comment($2);
-		} else {
-		    # Need more data to get all comment text.  This might
-		    # result in the comment callback being called more than
-		    # once for the several comment data.
-		    $$buf = $eaten . $$buf;
-		    return $self;
-		}
-	    }
-	    # Can we finish the tag
-	    if ($$buf =~ s|^([^>]*)>||) {
-		$text .= $1;
-		$self->declaration($text) if $text =~ /\S/;
-	    } else {
-		$$buf = $eaten . $$buf;  # must start with it all next time
-		return $self;
-	    }
-        # Should we look for 'processing instructions' <? ...> ??
-	#} elsif ($$buf =~ s|<\?||) {
-	    # ...
-	# Then, look for a end tag
-	} elsif ($$buf =~ s|^</||) {
-	    # end tag
-	    if ($$buf =~ s|^\s*([a-z][a-z0-9\.\-]*)\s*>||i) {
-		$interp->end_it(lc($1));
-	    } elsif ($$buf =~ s|^\s*>||i) {
-		$interp->end_it('', 1);	# Empty end tag
-	    } elsif ($$buf =~ m|^\s*[a-z]*[a-z0-9\.\-]*\s*$|i) {
-		$$buf = "</" . $$buf;  # need more data to be sure
-		return $self;
-	    } else {
-		# it is plain text after all
-		$interp->resolve($$buf);
-		$$buf = "";
-	    }
-	# Then, finally we look for a start tag
-	} elsif ($$buf =~ s|^<||) {
-	    # start tag
-	    my $eaten = '<';
-
-	    # This first thing we must find is a tag name.  RFC1866 says:
-	    #   A name consists of a letter followed by letters,
-	    #   digits, periods, or hyphens. The length of a name is
-	    #   limited to 72 characters by the `NAMELEN' parameter in
-	    #   the SGML declaration for HTML, 9.5, "SGML Declaration
-	    #   for HTML".  In a start-tag, the element name must
-	    #   immediately follow the tag open delimiter `<'.
-	    if ($$buf =~ s|^(([a-zA-Z][a-zA-Z0-9\.\-]*)\s*)||) {
-		$eaten .= $1;
-		my $tag = lc $2;
-		my @attr;
-
-		# Then we would like to find some attributes
-		while ($$buf =~ s|^(([a-zA-Z][a-zA-Z0-9\.\-]*)\s*)||) {
-		    $eaten .= $1;
-		    my $attr = lc $2;
-		    my $val;
-		    # The attribute might take an optional value (first we
-		    # check for an unquoted value)
-		    if ($$buf =~ s|(^=\s*([^\"\'>\s][^>\s]*)\s*)||) { #"
-			$eaten .= $1;
-			$val = $2;
-		    # or quoted by " "or ' '
-		    } elsif ($$buf =~ s|(^=\s*([\"\'])(.*?)\2\s*)||s) { #"
-			$eaten .= $1;
-			$val = $3;
-                    # truncated just after the '=' or inside the attribute
-		    } elsif ($$buf =~ m|^(=\s*)$| or
-			     $$buf =~ m|^(=\s*[\"\'].*)|s) { #"
-			$$buf = "$eaten$1";
-			return $self;
-		    } else {
-			# assume attribute with implicit value
-			$val = $attr;
-		    }
-		    push(@attr, $attr, $val);
-		    #$attr{$attr}=$val
-		}
-
-		# At the end there should be a closing ">"
-		if ($$buf =~ s|^>||) {
-		    $interp->start_tag($tag, \@attr);
-		} elsif (length $$buf) {
-		    # Not a conforming start tag, regard it as normal text
-		    $interp->resolve($eaten);
-		} else {
-		    $$buf = $eaten;  # need more data to know
-		    return $self;
-		}
-
-	    } elsif (length $$buf) {
-		$interp->resolve($eaten);
-	    } else {
-		$$buf = $eaten . $$buf;  # need more data to parse
-		return $self;
-	    }
-
-	} elsif (length $$buf) {
-	    die; # This should never happen
-	} else {
-	    # The buffer is empty now
-	    return $self;
-	}
-    }
-    $self;
-}
-
-============================================================= */
 }
