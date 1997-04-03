15b9be662df9f6a2b1b9fd8de48297186b8f3970
steve <>
1997-04-03 21:57:17 +0000

    Have to setgid before setuid because after that it's not superuser anymore.

--- src/app/setuid/be_pia.c
+++ src/app/setuid/be_pia.c
@@ -10,11 +10,12 @@ main(int argc, char** argv, char** env)
   setenv("LOGNAME", "pia", 1);		/* doesn't work with /bin/bash */
   setenv("HOME", pw->pw_dir, 1);
 
+  /* Could do this with setregid, setreuid */
+  setgid(pw->pw_gid);			/* Have to do this first. */
   if (setuid(pw->pw_uid)) {
     printf("sorry\n");
     exit(-1);
   }
-  setgid(pw->pw_gid);	
   argv[0] = "-";
   /* pw->pw_shell doesn't work if it's set to "false" to prevent logins */
   execve ("/bin/bash", argv, env);	
