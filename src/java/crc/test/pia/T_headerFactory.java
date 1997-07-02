68f258865671a8921df8e5d460581056ba59e9a8
rithy <>
1997-07-02 21:13:12 +0000

    test class for headerFactory

---
+++ src/java/crc/test/pia/T_headerFactory.java
@@ -0,0 +1,55 @@
+// T_headerFactory.java
+// $Id$
+// (c) COPYRIGHT Ricoh California Research Center, 1997.
+package crc.test.pia;
+
+import java.io.InputStream;
+import crc.pia.HeaderFactory;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import crc.pia.Headers;
+import crc.pia.Pia;
+
+/**  HeaderFactoryTest
+ * creates an appropriate header object from a HTTP stream
+ */
+public class T_headerFactory
+{
+
+  private static void usage(){
+    System.out.println("Test the creation of a header.  Use headerstest.txt for input.");
+    System.out.println("java crc.pia.HeaderFactory headerstest.txt");
+  }
+
+  /**
+  * For testing.
+  * 
+  */ 
+  public static void main(String[] args){
+    if( args.length == 0 ){
+      usage();
+      System.exit( 1 );
+    }
+
+    String filename = args[0];
+
+    HeaderFactory hf = new HeaderFactory();
+
+    try{
+      InputStream in = (new BufferedInputStream
+			(new FileInputStream (filename)));
+    
+      Headers h = hf.createHeader( in );
+      Pia.debug( h.toString() );
+    }catch(Exception e ){
+      Pia.debug( e.toString() );
+    }finally{
+      System.exit( 0 );
+    }
+  }
+}
+
+
+
+
+
