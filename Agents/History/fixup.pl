69b14137826aefaab434afaffe6ee287a50812bb
steve <>
1997-07-15 00:54:26 +0000

    Move History files from yymmdd.html to yyyy/mm/dd.html. Add fixup.pl and documentation.

---
+++ Agents/History/fixup.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+### fixup.pl -- move yymmdd.html to yyyy/mm/dd.html
+###             in $HOME/.pia/History 
+### === should really take the directory to hack on the command line ===
+
+$home = $ENV{'HOME'};
+chdir("$home/.pia/History");
+
+if (opendir(DIR, ".")) {
+    @names = readdir(DIR);
+    closedir(DIR);
+} else {
+    die "No directory ~/.pia/History";
+}
+
+foreach $fn (@names) {
+    if ($fn =~ /([0-9][0-9])([0-9][0-9])([0-9][0-9]).html/) {
+	my ($yy, $mm, $dd) = ($1, $2, $3);
+	mkdir ("19$yy", 0777);
+	mkdir ("19$yy/$mm", 0777);
+	print $fn, " -> 19$yy/$mm/$dd.html\n";
+	rename ($fn, "19$yy/$mm/$dd.html");
+    } else {
+	print "$fn not a history file\n";
+    }
+}
+
