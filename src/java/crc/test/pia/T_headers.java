3ed5cd344263c96df8393edc7c841179d0ffed58
rithy <>
1997-08-21 23:27:40 +0000

    add more comment.

--- src/java/crc/test/pia/T_headers.java
+++ src/java/crc/test/pia/T_headers.java
@@ -8,6 +8,10 @@ import crc.pia.Headers;
 
 public class T_headers {
   public static void main(String[] args){
+    System.out.println("Test creating a header from the Header class.");
+    System.out.println("Input is done by using set operators(setHeader, setContentType,...).");
+    System.out.println("Output is a dump of the created header.\n\n");
+
     try{
       Headers h = new Headers();
       h.setHeader("Host", "napa.crc.ricoh.com:9999");
