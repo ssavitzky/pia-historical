b188670bb72a8c25c7b187479331817127023582
steve <>
1997-09-20 00:54:57 +0000

    Implemented timed submission (but requires Crontab)

--- src/java/crc/interform/handle/Submit_forms.java
+++ src/java/crc/interform/handle/Submit_forms.java
@@ -13,6 +13,7 @@ import crc.interform.Run;
 import crc.sgml.SGML;
 import crc.sgml.Tokens;
 import crc.sgml.Text;
+import crc.sgml.Element;
 
 import crc.pia.Agent;
 import java.util.Enumeration;
@@ -61,29 +62,47 @@ public class Submit_forms extends crc.interform.Handler {
  
   public void handle(Actor ia, SGML it, Interp ii) {
     String name = Util.getString(it, "agent", null);
-    
+    SGML itt = containsTimedSubmission(it)? it : null;
+
     Run env = Run.environment(ii);
     crc.pia.Agent a = (name == null)? env.agent : env.getAgent(name);
 
-    // === timedSubmission unimplemented ===
-
-    if ( it.tag().equalsIgnoreCase("form") ){
-      String url = it.attrString("action");
-      String method = it.attrString("method");
-      a.createRequest(method, url, trimQuery(formToQuery(it)));
-    } else if (it.hasAttr("href")) {
-      String url = it.attrString("href");
-      a.createRequest("GET", url, null);
+    if (it.tag().equalsIgnoreCase("form") || it.hasAttr("href") ){
+      submit(a, it, itt);
     } else {
-      handleContent(a, it);
+      handleContent(a, it, itt);
     }
   }
 
-  public void handleContent(Agent a, SGML it) {
+  /** Submit a form or request using an agent.
+   * 	@param a	the agent submitting the form.
+   *	@param it	the form to be submitted
+   *	@param itt	a token with timed-submission attributes.
+   */
+  protected void submit(Agent a, SGML it, SGML itt) {
     if ( it.tag().equalsIgnoreCase("form") ){
       String url = it.attrString("action");
       String method = it.attrString("method");
-      a.createRequest(method, url, formToQuery(it));
+      if (itt == null) 
+	a.createRequest(method, url, trimQuery(formToQuery(it)));
+      else 
+	a.createTimedRequest(method, url, trimQuery(formToQuery(it)), itt);
+    } else if (it.hasAttr("href")) {
+      String url = it.attrString("href");
+      if (itt == null) 
+	a.createRequest("GET", url, null);
+      else 
+	a.createTimedRequest("GET", url, null, itt);
+    }
+  }
+
+  /** Look for forms in the content.  Recursively enumerates the content,
+   *	processing any forms it finds.  Probably confused by forms that
+   *	submit themselves via the <code>submit</code> attribute.
+   */
+  protected void handleContent(Agent a, SGML it, SGML itt) {
+    if ( it.tag().equalsIgnoreCase("form") ){
+      submit(a, it, itt);
     } else { 
       Tokens content = it.content();
       if (content != null){
@@ -91,13 +110,15 @@ public class Submit_forms extends crc.interform.Handler {
 	while( tokens.hasMoreElements() ){
 	  try{
 	    SGML e = (SGML)tokens.nextElement();
-	    handleContent(a, e);
+	    handleContent(a, e, itt);
 	  }catch(Exception excep){}
 	}
       }
     }
   }
 
+  /** Convert a form to a query string.
+   */
   public String formToQuery(SGML it) {
     String query = "";
 
@@ -112,7 +133,13 @@ public class Submit_forms extends crc.interform.Handler {
     } else if ("select".equalsIgnoreCase(it.tag())) {
       // === select unimplemented
     } else if ("textarea".equalsIgnoreCase(it.tag())) {
-      // === textarea unimplemented
+      // === textarea untested
+      query = it.attrString("name");
+      if (query == null || "".equals(query)) return "";
+      query = query.toLowerCase();
+      query += "=";
+      query += java.net.URLEncoder.encode(it.contentString());
+      query += "&";		// in case there's a next one.
     } else {
       Tokens content = it.content();
       if (content == null) return query;
@@ -126,11 +153,26 @@ public class Submit_forms extends crc.interform.Handler {
     return query;
   }
 
-  public String trimQuery(String query) {
+  /** trim an extraneous &amp; from the end of a query string. */
+  protected static String trimQuery(String query) {
     if (query.endsWith("&")) 
       query = query.substring(0, query.length()-1);
     return query;
   }
+
+  /** Attributes that determine a timed submission */
+  protected static String timeAttrs[] = {
+    "repeat", "until", "hour", "minute", "day", "month", "weekday" 
+  };
+
+  /** Determine whether a form is timed. */
+  protected static boolean containsTimedSubmission(SGML it) {
+    for (int i = 0; i < timeAttrs.length; ++i) {
+      if (it.hasAttr(timeAttrs[i])) return true;
+    }
+    return false;
+  } 
+
 }
 
 /* ====================================================================
