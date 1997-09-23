7c41e177e5c2d9ff9a3812fd1ec10e3c5058b22d
rithy <>
1997-09-23 00:07:46 +0000

    yank test code out and fix bugs

--- src/java/crc/ds/Index.java
+++ src/java/crc/ds/Index.java
@@ -202,11 +202,12 @@ public class Index {
       items = Util.split(path,'.');
       if( path.endsWith(".") && !path.endsWith("..") ){
 	items.push("");
-	System.out.println("Index-->pushing a blank, end in dot");
+	Pia.debug(this, "Index-->pushing a blank, end in dot");
       }
     }
     this.path = path;
   }
+
   /*
   public Index(List l) {
     items = l; // we don't manipulate items, so no need to copy
@@ -236,8 +237,6 @@ public class Index {
 	processState = DOTS;
 	return DOTS;
     }
-
-    //System.out.println("nextPosition's item: "+item+ " length-->"+ Integer.toString(item.length()));
 
     StringTokenizer ts = new StringTokenizer(item, "-", true);
     try{
@@ -282,7 +281,7 @@ public class Index {
     while( !tmp.isEmpty() ){
       o = tmp.shift();
       if( o != null )
-	System.out.println("[" + (String) o + "]");
+	Pia.debug(this, "dumpPositionParam-->[" + (String) o + "]");
     }
   }
   
@@ -318,10 +317,10 @@ public class Index {
   }
 
   protected void parseTag(){
-    //System.out.println( "Entering parseTag...");
+    //Pia.debug( "Entering parseTag...");
 
     String t = nextToken();
-    //System.out.println("Token-->"+t);
+    //Pia.debug("Token-->"+t);
 
     // missing tag and dash, tag is ANY.  current token is number
     if( parseNumber( t ) != INVALIDNUMBER ){
@@ -338,17 +337,17 @@ public class Index {
       tag = t;
     }
 
-    //System.out.println("Exiting parseTag...");
+    //Pia.debug("Exiting parseTag...");
   }
 
   protected void parseStart() throws InvalidInput{
     int number;
-    //System.out.println("Entering parseStart...");
+    //Pia.debug("Entering parseStart...");
 
     //dumpPositionParam();
     // getting dash
     String t = nextToken();
-    //System.out.println("Starting token-->"+t);
+    //Pia.debug("Starting token-->"+t);
     // no positions are specified
     if( t == null ){
       setStart( FIRST );
@@ -364,7 +363,7 @@ public class Index {
     if( isDash( t ) ){
       // get start position
       t = nextToken();
-      //System.out.println("Token-->"+t);
+      //Pia.debug("Token-->"+t);
       
       // this should be number or empty
       if ( (number = parseNumber( t )) != INVALIDNUMBER ){
@@ -385,7 +384,7 @@ public class Index {
       throw new InvalidInput("Invalid starting index.");
     }
 
-    //System.out.println(this, "Exiting parseStart...");
+    //Pia.debug(this, "Exiting parseStart...");
   }
 
   protected void parseEnd() throws InvalidInput{
@@ -423,9 +422,9 @@ public class Index {
       parseStart();
       parseEnd();
 
-      System.out.println("The tag-->"+ getTag());
-      System.out.println("The start-->"+ Integer.toString(getStart()));
-      System.out.println("The end-->"+ Integer.toString(getEnd()));
+      Pia.debug("The tag-->"+ getTag());
+      Pia.debug("The start-->"+ Integer.toString(getStart()));
+      Pia.debug("The end-->"+ Integer.toString(getEnd()));
 
       s = getStart();
       e = getEnd();
@@ -435,7 +434,7 @@ public class Index {
 	throw new InvalidInput("Start index is larger than end index."); 
 
     }catch(InvalidInput ee){
-      //System.out.println("The error is-->"+ee.toString());
+      //Pia.debug("The error is-->"+ee.toString());
       throw ee;
     }
   }
@@ -460,7 +459,7 @@ public class Index {
     String myTag = getTag();
 
     if( s.isToken() ) {
-      System.out.println("In merge -- token case "+s.toString());
+      Pia.debug("In merge -- token case "+s.toString());
       return s;
     }
     else{
@@ -469,7 +468,7 @@ public class Index {
       else{
 	if( s.content().nItems() > 0 ){
 	  Class c = (s.content().itemAt( 0 )).getClass();
-	  System.out.println("the class-->"+c.toString());
+	  Pia.debug("the class-->"+c.toString());
 	  try{
 	    result = (SGML)c.newInstance();
 	  }catch(Exception e){e.toString();}
@@ -479,7 +478,7 @@ public class Index {
 
       result.append( s.content() );	
 
-      System.out.println("In merge -- tokens case "+result.toString());
+      Pia.debug("In merge -- tokens case "+result.toString());
     }
     return result;
   }
@@ -488,16 +487,16 @@ public class Index {
     try{
       if( isDots() ){
 	// either . or ..
-	System.out.println("processing dots case.");
+	Pia.debug("processing dots case.");
 	datum = merge( datum );
       }else{
 	// normal indexing case
-	System.out.println("processing normal case.-->"+ datum.toString());
+	Pia.debug("processing normal case.-->"+ datum.toString());
 	parsePositions();
         datum = datum.attr( this );
       }
     }catch(InvalidInput e){
-      //System.out.println(e.toString());
+      //Pia.debug(e.toString());
       throw e;
     }
 
@@ -513,7 +512,7 @@ public class Index {
     int what;
 
     if( datum == null ) throw new IllegalArgumentException("Datum is null.");
-    System.out.println("lookup-->"+datum.toString());
+    Pia.debug("lookup-->"+datum.toString());
 
     what = nextPositions();
     
@@ -521,7 +520,7 @@ public class Index {
       try{
 	datum = doProcess( what, datum ); 
       }catch( InvalidInput e ){
-	System.out.println(e.toString());
+	Pia.debug(e.toString());
 	throw e;
       }
       resetPositionAttr();
@@ -529,17 +528,17 @@ public class Index {
     }
     
     
-    System.out.println("my class is-->"+datum.getClass().toString());
-    System.out.println("my content is-->"+Integer.toString(datum.content().nItems()));
+    Pia.debug("my class is-->"+datum.getClass().toString());
+    Pia.debug("my content is-->"+Integer.toString(datum.content().nItems()));
 
     if( datum instanceof Tokens && datum.content().nItems() == 1 ){
       // datum is a Tokens.  return a Token instead with the datum's content
       
-      System.out.println("Datum is a Tokens with 1 content");
+      Pia.debug("Datum is a Tokens with 1 content");
       
       SGML element = datum.content().itemAt(0);
       if( element.isToken())
-	System.out.println("Datum is return as a Token");
+	Pia.debug("Datum is return as a Token");
 
       return element;
     }
@@ -622,14 +621,14 @@ public class Index {
 
     SGML foobar = new Element();
 
-    //System.out.println("In path=========================");
+    //Pia.debug("In path=========================");
 
     int isDotDot = path.indexOf("..");
 
     // we don't want ..
     if( isDotDot != -1 ) throw new InvalidInput("dot dot is not accepted.");
     if( datum == null ) throw new IllegalArgumentException("Datum is null.");
-    System.out.println("original datum-->"+datum.toString());
+    Pia.debug("original datum-->"+datum.toString());
 
     what = nextPositions();
     
@@ -639,21 +638,21 @@ public class Index {
 
       cur = doProcess( what, cur ); 
 
-      if( cur.content().nItems() == 0 ){
-	System.out.println(" not successfull "+prev.getClass().toString());
+      if( cur == null || cur.content().nItems() == 0 ){
+	Pia.debug(" not successfull "+prev.getClass().toString());
 	
-	if( prev.content().itemAt(0).toString().equalsIgnoreCase("") )
+	if( prev.content() == null || prev.content().itemAt(0).toString().equalsIgnoreCase("") )
 	  whereToPut = prev;
-	else if( prev instanceof Element ){
-	  System.out.println("************");
+	else if( !(prev instanceof Tokens) ){
+	  Pia.debug("************");
 	  whereToPut = prev;
 	}
 	else{
 	  cont    = prev.content();
-	  System.out.println("The current cont is-->"+cont.toString());
+	  Pia.debug("The current cont is-->"+cont.toString());
 	  
 	  contNum = cont.nItems();
-	  System.out.println("The contNum is-->"+Integer.toString(contNum));
+	  Pia.debug("The contNum is-->"+Integer.toString(contNum));
 	  
 	  if( contNum == 1 )
 	    whereToPut = prev.content().itemAt(0);
@@ -664,34 +663,34 @@ public class Index {
 	  else whereToPut = prev;  
         }
 	
-	System.out.println("where to put-->"+whereToPut.toString());
-	System.out.println("tag-->"+getTag());
-	System.out.println("start-->"+Integer.toString( getStart() ));
+	Pia.debug("where to put-->"+whereToPut.toString());
+	Pia.debug("tag-->"+getTag());
+	Pia.debug("start-->"+Integer.toString( getStart() ));
 	
-	System.out.println(" before get tag.");
+	Pia.debug(" before get tag.");
 	if ( getTag().equalsIgnoreCase( ANY ))
 	  newTag = "";
 	else
 	  newTag = getTag();
 	newElement = new Element( newTag, "" );
-	System.out.println("after create element-->"+newElement.toString());
+	Pia.debug("after create element-->"+newElement.toString());
 
-	if( getStart() >= whereToPut.content().nItems() ){
+	if( whereToPut.content() != null && getStart() >= whereToPut.content().nItems() ){
 	  whereToPut.append( newElement );
-	  System.out.println("In append case greater or equal");
+	  Pia.debug("In append case greater or equal");
 	}
 	else
 	  whereToPut.append( newElement );
 	
 	cur = newElement;
 	foobar.append( whereToPut );	
-	System.out.println("the cur path is-->"+whereToPut.toString());
+	Pia.debug("the cur path is-->"+whereToPut.toString());
       }
 
       resetPositionAttr();
       what = nextPositions(); 
       }
-    System.out.println("original datum-->"+datum.toString());
+    Pia.debug("original datum-->"+datum.toString());
     return cur;
   }
 
@@ -723,180 +722,6 @@ public class Index {
       //return (SGML)data.at(path);
   }
 
-  public static SGML makeHtml0(){
-    SGML foo = new Element();
-    
-    SGML u1 = new Element("ul");
-    SGML u2 = new Element("ul"); 
-    
-    SGML c1 = new Element("li");
-    c1.append("Hello world");
-    
-    SGML c2 = new Element("li");
-    c2.append("I love u. U love me");
-    
-    u1.append( c1 );
-    u2.append( c2 );
-    foo.append( u1 );
-    foo.append( u2 );
-
-    return foo;
-  }
-  
-  public static SGML makeHtml1(){
-    SGML foo = new Element();
-    SGML dl = new DescriptionList(new Element("dl"));
-    
-    SGML dt1 = new Element("dt", "aaa");
-    SGML dt2 = new Element("dt", "bbb"); 
-    
-    SGML dd1 = new Element("dd", "no sex");
-    SGML dd2 = new Element("dd", "in nirvana");
-    
-    dl.append( dt1 );
-    dl.append( dd1 );
-    dl.append( dt2 );
-    dl.append( dd2 );
-
-    foo.append( dl );
-
-    return foo;
-  }
-
-  public static SGML makeHtml2(){
-    SGML foo = new Element();
-    SGML dl = new DescriptionList(new Element("dl"));
-    
-    SGML dt1 = new Element("dt", "aaa");
-    SGML dt2 = new Element("dt", "bbb"); 
-    
-    SGML dd1 = new Element("dd", "no sex");
-    SGML dd2 = new Element("dd", "in nirvana");
-    
-    //dl.append( dt1 );
-    dl.append( dd1 );
-    dl.append( dt2 );
-    dl.append( dd2 );
-
-    foo.append( dl );
-
-    return foo;
-  }
-
-  public static SGML makeHtml3(){
-    SGML foo = new Element();
-    SGML dl = new DescriptionList(new Element("dl"));
-    
-    SGML dt1 = new Element("dt", "aaa");
-    SGML dt2 = new Element("dt", "bbb"); 
-    
-    SGML dd1 = new Element("dd", "no sex");
-    SGML dd2 = new Element("dd", "in nirvana");
-    
-    dl.append( dt1 );
-    //dl.append( dd1 );
-    dl.append( dt2 );
-    dl.append( dd2 );
-
-    foo.append( dl );
-
-    return foo;
-  }
-
-
-  public static SGML makeHtml4(){
-    SGML foo = new Element();
-    SGML dl = new DescriptionList(new Element("dl"));
-    
-    SGML dt1 = new Element("dt", "aaa");
-    SGML dt2 = new Element("dt", "bbb"); 
-    
-    SGML dd1 = new Element("dd", "no sex");
-    SGML dd2 = new Element("dd", "in nirvana");
-    SGML dd3 = new Element("dd", "Barney is a dork");
-    
-    dl.append( dt1 );
-    dl.append( dd1 );
-    dl.append( dd2 );
-    dl.append( dt2 );
-    dl.append( dd3 );
-
-    foo.append( dl );
-
-    return foo;
-  }
-  
-  public static SGML makeHtml5(){
-    SGML foo = new Element();
-    SGML dl = new DescriptionList(new Element("dl"));
-    
-    SGML dt1 = new Element("dt", "aaa");
-    SGML dt2 = new Element("dt", "aaa"); 
-    
-    SGML dd1 = new Element("dd", "no sex");
-    SGML dd2 = new Element("dd", "in nirvana");
-    
-    dl.append( dt1 );
-    dl.append( dd1 );
-    dl.append( dt2 );
-    dl.append( dd2 );
-
-    foo.append( dl );
-
-    return foo;
-  }
-
-  
-
-  public static void main(String argv[]){
-    String s = null;
-
-    if(argv.length <2 )
-      System.out.println("need doc# and path.");
-
-    SGML foo = new Text("");
-    int what = 0;
-    try{
-      what = Integer.parseInt( argv[0] );
-    }catch(Exception e){}
-
-    switch( what ){
-    case 0:
-      foo = makeHtml0();
-      break;
-    case 1 :
-      foo = makeHtml1();
-      break;
-    case 2:
-      foo = makeHtml2();
-      break;
-    case 3:
-      foo = makeHtml3();
-      break;
-    case 4:
-      foo = makeHtml4();
-      break;
-    case 5:
-      foo = makeHtml5();
-      break;
-    default:
-      foo = new Element("table", "");
-      break;
-    }
-	
-    System.out.println("original foo-->"+foo.toString());
-
-    try{
-      //SGML result = Index.get(argv[1], foo);
-
-      Index i = new Index( argv[1] );
-      SGML result = i.path( foo );
-
-
-      System.out.println("Here is the result-->"+result.toString());
-    }catch(Exception e){
-    } 
-  }
 }
 
 
