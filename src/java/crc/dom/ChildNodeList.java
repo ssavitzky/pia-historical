0addd16376c42d567534a8c6d376bee78e482ef0
rithy <>
1998-02-12 19:45:46 +0000

    add implementation

--- src/java/crc/dom/ChildNodeList.java
+++ src/java/crc/dom/ChildNodeList.java
@@ -6,28 +6,166 @@ package crc.dom;
 
 import java.io.*;
 
-import w3c.dom.Node;
-import w3c.dom.NodeList;
-import w3c.dom.NodeEnumerator;
-import w3c.dom.NoSuchNodeException;
-
 /**
  * This list expands a children collection through the start node.
  */
 
 public class ChildNodeList implements NodeList {
 
-  public ChildNodeList(){}
-  public ChildNodeList(Node startNode){}
+  public ChildNodeList(){
+    header = new HeaderNode();
+    header.setPrevious( null );
+    header.setNext( null );
+  }
 
-  public NodeEnumerator getEnumerator(){ return null; }
+  public ChildNodeList(AbstractNode newChild){
+    header = new HeaderNode();
+
+    newChild.setPrevious( null );
+    newChild.setNext( null );
+
+    header.setPrevious( newChild );
+    header.setNext( newChild );
+
+    header.incCount();
+  }
+
+  public NodeEnumerator getEnumerator(){ return new ChildNodeListEnumerator( this ); }
 
   public Node item(long index)
-       throws NoSuchNodeException{ return null; }
+       throws NoSuchNodeException
+  {
+    long i = 0;
+    AbstractNode n = header.getPrevious();
+
+    if( index < header.getCount() - 1 && index >= 0 ){
+      while( i != index ){
+	n = n.getNext();
+      }
+      return n;
+    }
+    else{
+      String err = ("Index out of bound.");
+      throw new NoSuchNodeException(err);
+    }
+   
+  }
 
     /**
      * walk till end of child
      */
-  public long getLength(){ return 0; }
+  public long getLength(){ return header.getCount(); }
 
+
+  protected void doInsertAtStart(AbstractNode newChild, AbstractNode refChild){
+    if( newChild == null || refChild == null ) return;
+
+    AbstractNode temp = refChild.getPrevious();
+
+    newChild.setPrevious( temp );
+    newChild.setNext( refChild );
+
+    refChild.setPrevious( newChild );
+
+    temp.setPrevious( newChild );
+  }
+
+
+
+  protected void doInsertBefore(AbstractNode newChild, AbstractNode refChild){
+    if( newChild == null || refChild == null ) return;
+
+    if( refChild == header.getPrevious() ){
+      doInsertAtStart(newChild, refChild);      
+      header.incCount();
+      return;
+    }
+
+    AbstractNode temp = refChild.getPrevious();
+
+    newChild.setPrevious( temp );
+    newChild.setNext( refChild );
+
+    refChild.setPrevious( newChild );
+
+    temp.setNext( newChild );
+    header.incCount();
+  }
+
+  protected void doInsertLast( AbstractNode newChild){
+    if( header.getPrevious() == null && header.getNext() == null ){
+      header.setPrevious( newChild );
+      header.setNext( newChild );
+      header.incCount();
+      return;
+    }
+      
+    AbstractNode last = header.getNext();
+    
+    newChild.setPrevious( last );
+    newChild.setNext( last.getNext() );
+
+    last.setNext( newChild );
+
+    header.setNext( newChild );
+    header.incCount();
+  }
+
+
+  protected Node doRemoveChild( AbstractNode p ){
+    if( p == null ) return null;
+
+    if( header.getPrevious() == header.getNext() ){ // only one child
+      header.setPrevious( null );
+      header.setNext( null );
+    }else if( header.getNext() == p ){// remove the last child, should adjust header's next reference
+      p.getPrevious().setNext( p.getNext() );
+      header.setNext( p.getPrevious() ); 
+    }else if( header.getPrevious() == p ){ // the first item, but more than one child
+      p.getPrevious().setPrevious( p.getNext() );
+      p.getNext().setPrevious( header );
+    }else{
+      p.getPrevious().setNext( p.getNext() );
+      p.getNext().setPrevious( p.getPrevious() );
+    }
+    p.setPrevious( null );
+    p.setNext( null );
+    header.decCount();
+    return p;
+
+  }
+
+
+  protected Node doReplaceChild(AbstractNode oldChild, AbstractNode newChild){
+    if( oldChild == null || newChild == null ) return null;
+    
+    AbstractNode previous = oldChild.getPrevious();
+    AbstractNode next = oldChild.getNext();
+    
+    newChild.setPrevious( previous );
+    newChild.setNext( next );
+
+    oldChild.setPrevious (null );
+    oldChild.setNext ( null );
+
+    // more than one item in the list, adjust header's previous reference
+    if( header.getPrevious() != header.getNext() ){
+      header.setPrevious( newChild );
+    }else if ( oldChild == header.getPrevious() ){ // only one item
+      header.setPrevious( newChild );
+      header.setNext( newChild );
+    }else if ( oldChild == header.getNext() ){ // replacing the last item
+      header.setNext( newChild );
+    }
+
+    return oldChild;
+  }
+
+  /**
+   * Header node where previous points to start and next points to last.
+   * Make list circular
+   */
+  protected HeaderNode header;
 }
+
+
--- src/java/crc/dom/ChildNodeListEnumerator.java
+++ src/java/crc/dom/ChildNodeListEnumerator.java
@@ -11,35 +11,107 @@ package crc.dom;
 
 import java.io.*;
 
-import w3c.dom.Node;
-import w3c.dom.NodeEnumerator;
-
-
 public class ChildNodeListEnumerator implements NodeEnumerator {
 
-  public ChildNodeListEnumerator(ChildNodeList l){
-    cursor = 0;
+  public ChildNodeListEnumerator(ChildNodeList list) throws NullPointerException{
+    if ( list == null ){
+      String err = ("Illegal list.");
+      throw new NullPointerException(err);
+    }
+    l = list;
+    cursor = l.header.getPrevious();
   }
 
-  public Node getFirst(){ return null; }
-  public Node getNext(){ return null; }
-  public Node getPrevious(){ return null; }
-  public Node getLast(){ return null; }
+  /**
+   *Returns the first node that the enumeration refers to, and resets the
+   *enumerator to reference the first node. If there are no nodes in the
+   *enumeration, null is returned. 
+   */
+  public Node getFirst(){
+    cursor = l.header.getPrevious();
+    return cursor;
+  }
 
-  public Node getCurrent(){ return null; }
+  /**
+   *Return the next node in the enumeration, and advances the enumeration. Returns
+   *null after the last node in the list has been passed, and leaves the current
+   *pointer at the last node. 
+   */
+  public Node getNext(){ 
+    if( cursor == l.header.getNext() )
+      return null;
 
-  // The rationale for their existence is that the enumerator may be used
-  // internally to a method, which may return some interesting value, and
-  // therefore cannot also indicate whether the start or end of enumeration
-  // was reached.  Any of the traversal methods affects the state, and
-  // so are not suitable for usage as predicates (unless possible state
-  // manipulation is acceptable).
+    cursor = cursor.getNext();
+    return cursor;
+  }
 
-  public boolean atStart(){ return false; }
-  public boolean atEnd(){ return false; }
+  /**
+   *Return the previous node in the enumeration, and regresses the enumeration.
+   *Returns null after the first node in the enumeration has been returned, and
+   *leaves the current pointer at the first node. 
+   */
+  public Node getPrevious(){
+    if( cursor == l.header.getPrevious() )
+      return null;
 
-  protected int cursor;
+    cursor = cursor.getPrevious();
+    return cursor;
+  }
 
+  /**
+   *Returns the last node in the enumeration, and sets the enumerator to reference
+   *the last node in the enumeration. If the enumeration is empty, this method will
+   *return null. Doing a getNext() immediately after this operation will return null. 
+   */
+
+  public Node getLast(){
+    if( isEmpty() )
+      return null;
+
+    cursor = l.header.getNext();
+    return cursor;
+  }
+
+  /**
+   * This returns the node that the enumeration is currently referring to, without
+   * affecting the state of the enumeration object in any way. When invoked before
+   * any of the enumeration positioning methods above, the node returned will be the
+   * first node in the enumeration, or null if the enumeration is empty. 
+   */
+  public Node getCurrent(){
+    return cursor; 
+  }
+
+  /**
+   *Returns true if the enumeration's "pointer" is positioned at the start of the
+   *set of nodes, i.e. if getCurrent() will return the same node as getFirst() would
+   *return. For empty enumerations, true is always returned. Does not affect the
+   *state of the enumeration in any way. 
+   */
+  public boolean atStart(){
+    if( isEmpty() ) return true;
+
+    return cursor == l.header.getPrevious();
+  }
+
+  /**
+   * Returns true if the enumeration's "pointer" is positioned at the end of the set
+   *  of nodes, i.e. if getCurrent() will return the same node as getLast() would return.
+   *  For empty enumerations, true is always returned. Does not affect the state of
+   *  the enumeration in any way. 
+   */
+
+  public boolean atEnd(){
+    if ( isEmpty() ) return true;
+    return cursor == l.header.getNext();
+  }
+
+  protected boolean isEmpty(){ 
+    return l.header.getNext() == null && l.header.getPrevious() == null;
+  }
+
+  protected AbstractNode cursor = null;
+  protected ChildNodeList l;
 }
 
 
