e540b02b2a3e2cb1c44863936323c1c0a6d97790
rithy <>
1998-02-12 19:44:54 +0000

    implementation called from parser

--- src/java/crc/dom/CRCDOMFactory.java
+++ src/java/crc/dom/CRCDOMFactory.java
@@ -11,32 +11,38 @@ package crc.dom;
 
 import java.io.*;
 
-import w3c.dom.DOMFactory;
-import w3c.dom.Document;
-import w3c.dom.DocumentContext;
-import w3c.dom.Element;
-import w3c.dom.Text;
-import w3c.dom.Comment;
-import w3c.dom.PI;
-import w3c.dom.Attribute;
-import w3c.dom.AttributeList;
-import w3c.dom.NodeList;
-
-
-public abstract class CRCDOMFactory extends AbstractDOMFactory {
+public class CRCDOMFactory extends AbstractDOMFactory {
 
   public CRCDOMFactory(){}
 
   /**
    * implements DOMFactory interfaces
    */
-  public abstract Document createDocument();
-  public abstract DocumentContext   createDocumentContext();
-  public Element           createElement(String tagName, AttributeList attributes){ return null; }
+  public Document createDocument(){
+  Report.debug("createDocument");
+    return new AppDocument();
+  }
+
+  public DocumentContext   createDocumentContext(){ return null; }
+
+  public Element           createElement(String tagName, AttributeList attributes){
+    if( tagName == null ) return null;
+    Element e = new CRCElement();
+    e.setTagName( tagName );
+    if ( attributes != null )
+      e.setAttributes( attributes );
+    return e;
+  }
+
   public Text              createTextNode(String data){ return null; }
   public Comment           createComment(String data){ return null; }
   public PI                createPI(String name, String data){ return null; }
-  public Attribute         createAttribute(String name, NodeList value){ return null; }
+
+  /** create a basic attribute */
+  public Attribute         createAttribute(String name, NodeList value){
+    if( name == null ) return null;
+    return new CRCAttribute( name, value );
+  }
 
 }
 
