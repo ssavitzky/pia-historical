de24b59c640a00978c40664400c634b971ddd2c2
rithy <>
1998-02-14 01:44:56 +0000

    create CRCDocument in createDocument

--- src/java/crc/dom/CRCDOMFactory.java
+++ src/java/crc/dom/CRCDOMFactory.java
@@ -19,12 +19,17 @@ public class CRCDOMFactory extends AbstractDOMFactory {
    * implements DOMFactory interfaces
    */
   public Document createDocument(){
-  Report.debug("createDocument");
-    return new AppDocument();
+  Report.debug("Creating CRCDocument...");
+    return new CRCDocument();
   }
 
   public DocumentContext   createDocumentContext(){ return null; }
 
+  /**
+   *Create a CRCElement based on the tagName. Note that the instance returned may
+   *implement an interface derived from Element. The attributes parameter can be
+   *null if no attributes are specified for the new Element. 
+   */
   public Element           createElement(String tagName, AttributeList attributes){
     if( tagName == null ) return null;
     Element e = new CRCElement();
@@ -38,7 +43,7 @@ public class CRCDOMFactory extends AbstractDOMFactory {
   public Comment           createComment(String data){ return null; }
   public PI                createPI(String name, String data){ return null; }
 
-  /** create a basic attribute */
+  /** create a CRCAttribute which has a reference to AttributeDefinition */
   public Attribute         createAttribute(String name, NodeList value){
     if( name == null ) return null;
     return new CRCAttribute( name, value );
