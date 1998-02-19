36726c457f5ce5629c8eabaa9d507b53c20e87f5
rithy <>
1998-02-19 19:09:46 +0000

    previously Appcomment

---
+++ src/java/crc/dom/BasicComment.java
@@ -0,0 +1,29 @@
+// BasicComment.java
+// $Id$
+// (c) COPYRIGHT Ricoh California Research Center, 1998.
+
+
+/**
+ * Implements w3c's Document interface. 
+ */
+
+package crc.dom;
+
+import java.io.*;
+
+public class BasicComment extends AbstractNode implements Comment {
+
+  public BasicComment(){ comment = ""; }
+
+  /**
+   * implements DOMFactory interfaces
+   */
+  public int getNodeType(){ return NodeType.COMMENT; }
+  public void setData(String data){ comment = data; }
+  public String getData(){ return comment; }
+  protected String comment;
+}
+
+
+
+
