c65c3a350c09c711998c3365bda59a2a353a013b
rithy <>
1998-02-23 20:30:38 +0000

    now inherits from BasicDocument

--- src/java/crc/dom/CRCDocument.java
+++ src/java/crc/dom/CRCDocument.java
@@ -11,7 +11,7 @@ package crc.dom;
 
 import java.io.*;
 
-public class CRCDocument extends AbstractDocument implements Document {
+public class CRCDocument extends BasicDocument implements Document {
 
   /**
    * implements DOMFactory interfaces
@@ -23,48 +23,16 @@ public class CRCDocument extends AbstractDocument implements Document {
     setDocumentElement( null );
   }
 
-  public void setDocumentType(Node documentType){ this.documentType = documentType; }
-  public Node getDocumentType(){ return documentType; }
+  public CRCDocument(CRCDocument d)
+  {
+    super( d );
+  }
 
-  public void setDocumentElement(Element documentElement){ this.documentElement = documentElement; }
-  public Element getDocumentElement(){ return documentElement; }
+  public Object clone(){
+    CRCDocument n = (CRCDocument)super.clone();
+    return n;
+  }
 
-  /**
-   * Produces an enumerator which iterates over all of the Element nodes that are
-   *  contained within the document whose tagName matches the given name. The iteration
-   *  order is a depth first enumeration of the elements as they occurred in the
-   *  original document. 
-   */
-  public NodeEnumerator getElementsByTagName(String name){ return null; }
-
-  /* implementing Node interface */
-  public int getNodeType(){ return NodeType.DOCUMENT; }
-  public Node getParentNode(){ return null; }
-  public NodeList getChildren(){ return null; }
-  public boolean hasChildren(){ return false; }
-  public Node     getFirstChild(){ return null; }
-  public Node     getPreviousSibling(){ return null; }
-  public Node     getNextSibling(){ return null; }
-
-  public void insertBefore(Node newChild, Node refChild)
-       throws NotMyChildException{}
-
-  public Node replaceChild(Node oldChild, Node newChild)
-       throws NotMyChildException{ return null; }
-
-  public Node removeChild(Node oldChild)
-       throws NotMyChildException{ return null; }
-
-
-  /**
-   * document DTD.  This could be null for html document 
-   * If a document object has a DTD, then attributeDefinitions
-   * and entity references can be retrieve through DTD
-   */
-  protected Node documentType;
-
-  /* root node */
-  protected Element documentElement;
 
 }
 
