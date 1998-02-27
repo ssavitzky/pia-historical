7db1d516be59a550dbba06ab2755d402ac2ff90e
rithy <>
1998-02-27 23:52:36 +0000

    add mostly comment

--- src/java/crc/dom/ArrayNodeList.java
+++ src/java/crc/dom/ArrayNodeList.java
@@ -10,22 +10,23 @@ import java.util.Vector;
 
 /**
  * Mutable node collection.  This list could be used
- * to return accmulated links in a document.  Each node
+ * to return accumulated links in a document.  Each node
  * in this collection bears no relation to each other.
  */
 
 public class ArrayNodeList extends Vector implements EditableNodeList {
 
   public ArrayNodeList(){
-    //    nodeCollection = new Vector();
   }
 
+  /**
+   * Deep copy of another list.
+   */
   public ArrayNodeList(NodeList list)throws NullPointerException{
     if( list == null ){
       String err = ("Illegal list.");
       throw new NullPointerException(err);
     }
-    //nodeCollection = new Vector( (int)list.getLength() );
     initialize( list );
   }
 
@@ -33,7 +34,11 @@ public class ArrayNodeList extends Vector implements EditableNodeList {
    *Replace the indexth item the list with replacedNode, and return the old node
    *object at that index (null is returned if the index is equal to the previous
    *number of nodes in the list). If index is greater than the number of nodes in
-   *the list, a NoSuchNodeException is thrown. 
+   *the list, a NoSuchNodeException is thrown.
+   *@param index Ranges from 0 to size - 1
+   *@param replaceNode new node to be put at index
+   *@return previous node at index
+   *@exception NoSuchNodeException when indexed out of bound.
    */
   public Node replace(long index,Node replacedNode) 
        throws NoSuchNodeException
@@ -52,6 +57,11 @@ public class ArrayNodeList extends Vector implements EditableNodeList {
    * index to the end of list are moved up by one. If index is 0, the node is added
    * at the beginning of the list; if index is self.getLength(), the node is added at the
    * end of the list. 
+   * @param index If 0, the node is added
+   * at the beginning of the list; if index is self.getLength(), the node is added at the
+   * end of the list. 
+   * @param newNode new node to be put at index
+   * @exception NoSuchNodeException when indexed out of bound.
    */
   public void insert(long index,Node newNode) 
        throws NoSuchNodeException
@@ -74,7 +84,10 @@ public class ArrayNodeList extends Vector implements EditableNodeList {
    *Removes the node at index from the list and returns it. The indices of the
    *members of the list which followed this node are decremented by one following
    *the removal. If the index is provided is larger than the number of nodes in the
-   *list, the NoSuchNodeException is thrown. 
+   *list, the NoSuchNodeException is thrown.
+   *@param index Ranges from 0 to size - 1
+   *@return node at index
+   *@exception NoSuchNodeException when indexed out of bound.
    */
   public Node remove(long index)
        throws NoSuchNodeException
@@ -89,6 +102,9 @@ public class ArrayNodeList extends Vector implements EditableNodeList {
     return n;
   }
 
+  /**
+   * @return NodeEnumerator
+   */
   public NodeEnumerator getEnumerator()
   {
     return new ArrayNodeListEnumerator( this );
@@ -97,6 +113,9 @@ public class ArrayNodeList extends Vector implements EditableNodeList {
   /**
    *Returns the indexth item in the collection. If index is greater than or equal
    *to the number of nodes in the list, a NoSuchNodeException is thrown. 
+   *@param index Ranges from 0 to size - 1
+   *@return node at index
+   *@exception NoSuchNodeException when indexed out of bound.
    */
   public Node item(long index)
        throws NoSuchNodeException
@@ -116,7 +135,9 @@ public class ArrayNodeList extends Vector implements EditableNodeList {
    */
   public long getLength(){ return size(); }
   
-
+  /**
+   * Deep copy
+   */
   private void initialize( NodeList list ){
     NodeEnumerator e = list.getEnumerator();
 
