83f05a5a94352d3a2fe5ebcd38de17e3d65a0ded
rithy <>
1998-02-27 23:56:47 +0000

    remove debugging statements

--- src/java/crc/dom/BasicDOMFactory.java
+++ src/java/crc/dom/BasicDOMFactory.java
@@ -19,8 +19,8 @@ public class BasicDOMFactory extends AbstractDOMFactory {
    * implements DOMFactory interfaces
    */
   public Document createDocument(){
-  Report.debug("createDocument");
-    return new AppDocument();
+    //Report.debug(this,"createDocument");
+    return new BasicDocument();
   }
 
   public DocumentContext   createDocumentContext(){ return null; }
@@ -31,32 +31,37 @@ public class BasicDOMFactory extends AbstractDOMFactory {
    *null if no attributes are specified for the new Element. 
    */
   public Element           createElement(String tagName, AttributeList attributes){
+    //Report.debug(this,"createElement...");
     if( tagName == null ) return null;
     Element e = new BasicElement();
     e.setTagName( tagName );
-    if ( attributes != null )
+    if ( attributes != null ) 
       e.setAttributes( attributes );
+    return e;
   }
 
   /**
    *  Create a Text node given the specified string. 
    */
   public Text              createTextNode(String data){
-    return new AppText( data ); 
+    //Report.debug(this,"createTextNode...");
+    return new BasicText( data ); 
   }
 
   /**
    *  Create a Comment node given the specified string. 
    */
   public Comment           createComment(String data){
-    return new AppComment( data );
+    //Report.debug(this,"createComment...");
+    return new BasicComment( data );
   }
 
   /**
    *  Create a PI node with the specified name and data string.
    */
   public PI                createPI(String name, String data){
-    return new AppPI( name, data );
+    //Report.debug(this,"createPI...");
+    return new BasicPI( name, data );
   }
 
   /**
@@ -64,8 +69,9 @@ public class BasicDOMFactory extends AbstractDOMFactory {
    *Attribute instance can then be set on an Element using the setAttribute method.
    */
   public Attribute         createAttribute(String name, NodeList value){
+    //Report.debug(this,"createAttribute...");
     if( name == null ) return null;
-    return new BasicAttr( name, value );
+    return new BasicAttribute( name, value );
   }
 
 }
