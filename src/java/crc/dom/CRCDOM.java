083ebbaeb71160959939e4e5cf299562f8f0b5c9
rithy <>
1998-02-28 00:00:57 +0000

    Just create CRCDOMFactory

--- src/java/crc/dom/CRCDOM.java
+++ src/java/crc/dom/CRCDOM.java
@@ -13,9 +13,6 @@ import java.io.*;
 
 
 
-
-
-
 public class CRCDOM implements DOM {
 
   /**
@@ -23,16 +20,14 @@ public class CRCDOM implements DOM {
    * parameter to indicate which factory is desired.
    */
 
-  public DOMFactory getFactory(){ return null; }
+  public DOMFactory getFactory(){ return instance().factory; }
 
   /** Return the CRCDOM's only instance.  */
   public static CRCDOM instance() {
     if( instance != null )
       return instance;
     else{
-      String[] args = new String[1];
-      args[0] = HTML;
-      makeInstance(args);
+      makeInstance();
       return instance;
     }
   }
@@ -43,37 +38,18 @@ public class CRCDOM implements DOM {
   }
 
   /** Make a new instance */
-  private static void makeInstance(String[] args){
-    String docType = null;
-
+  private static void makeInstance(){
     /* Create a CRCDOM instance */
 
     CRCDOM crcdom = new CRCDOM();
-    crcdom.commandLine = args;
-    docType = args[0];
-
-    if( docType.equalsIgnoreCase(HTML) ){
-      factory = new CRCDOMFactory();
-    }else{
-      System.out.println("Can not create factory because of unkown document type.");
-      System.exit(1);
-    }
+    factory = new CRCDOMFactory();
   }
 
-  /** The command-line options passed to Java on startup.
-   */
-  private String[] commandLine;
-
-  /* reference to instance */
+   /* reference to instance */
   private static CRCDOM    instance	= null;
 
   /* reference to instance of dom factory */
-  private static AbstractDOMFactory factory = null;
-
-  /* kind of factory needed */
-  private static String HTML = "HTML";
-
-  
+  private static CRCDOMFactory factory = null;
 }
 
 
