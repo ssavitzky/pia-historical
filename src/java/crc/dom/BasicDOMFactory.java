da02fa3e7abf14d22ff745be8e40d5c21b38d3ce
rithy <>
1998-04-16 22:19:32 +0000

    create makedeepcopy

--- src/java/crc/dom/BasicDOMFactory.java
+++ src/java/crc/dom/BasicDOMFactory.java
@@ -74,6 +74,120 @@ public class BasicDOMFactory extends AbstractDOMFactory {
     return new BasicAttribute( name, value );
   }
 
+  /**
+   * deep copy
+   */
+  public Node makeDeepCopy(Node src){
+    Node des = doCopy( src );
+    if( des != null ){
+      copyChildren( src, des );
+      return des;
+    }
+    else
+      return null;
+  }
+
+  /**
+   * copy a node
+   */
+  public Node doCopy(Node org){
+    Node anode = null;
+
+    switch( org.getNodeType() ){
+    case NodeType.DOCUMENT:
+
+      Document oldDoc = (Document) org;
+      Element root = oldDoc.getDocumentElement();
+      Element newDocRoot = (Element)doCopy( root );
+      Document newDoc = createDocument();
+      newDoc.setDocumentElement( newDocRoot );
+
+      anode =  newDoc;
+      break;
+    case NodeType.ELEMENT:
+
+      Element e = (Element)org;
+      AttributeList attrList = new AttrList();
+      AttributeList ol = e.getAttributes();
+      Attribute a = null;
+      Attribute newAttr = null;
+
+      long len = ol.getLength();
+      for(long i=0; i < len; i++){
+	try{
+	  a = (Attribute)ol.item( i );
+	  newAttr = (Attribute)doCopy( a );
+	  if( newAttr != null )  
+	    attrList.setAttribute(a.getName(), newAttr);
+	}catch(NoSuchNodeException ee){}
+      }
+
+      anode = createElement( e.getTagName(), attrList );
+      break;
+    case NodeType.ATTRIBUTE:
+
+      Attribute attr = (Attribute)org;
+      EditableNodeList enl = new ArrayNodeList();
+      NodeList nl = attr.getValue();
+      Node n = null;
+      Node child = null;
+      
+      long length = nl.getLength();
+      for(long i=0; i < length; i++){
+	try{
+	  n = nl.item( i );
+	  child = doCopy( n );
+	  if( child != null )  
+	    enl.insert(enl.getLength(), child);
+	}catch(NoSuchNodeException ee){}
+      }
+      
+      anode = createAttribute( attr.getName(), enl );
+      break;
+    case NodeType.PI:
+      PI p = (PI)org;
+      anode = createPI( p.getName(), p.getData() );
+      break;
+    case NodeType.COMMENT:
+      Comment c = (Comment)org;
+      anode = createComment( c.getData() );
+      break;
+    case NodeType.TEXT:
+      Text t =(Text)org;
+      anode = createTextNode( t.getData() );
+      break;
+    case NodeType.ENTITY:
+      break;
+    default:
+      return null;
+    }
+
+    return anode;
+  }
+
+  protected void copyChildren(Node src, Node des){
+    Node elem = null;
+    Node child = null;
+
+
+    if( src.hasChildren() ){
+      NodeEnumerator enum = src.getChildren().getEnumerator();
+      
+      elem = enum.getFirst();
+      while( elem != null ){
+	child = doCopy( elem );
+	try{
+	  des.insertBefore( child, null );
+	}catch(NotMyChildException e){
+	  Report.debug(e.toString());
+	}
+      }
+      copyChildren( elem, child );
+      elem = enum.getNext();
+    }
+
+  }
+
 }
 
 
