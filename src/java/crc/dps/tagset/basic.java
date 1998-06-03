760be4b21e81e129e019e948a6e5d4e00b8b5615
steve <>
1998-06-03 22:09:22 +0000

    Renamed tagset classes to lowercase

---
+++ src/java/crc/dps/tagset/basic.java
@@ -0,0 +1,43 @@
+////// basic.java:  Initializer for Basic tagset
+//	$Id$
+//	Copyright 1997, Ricoh California Research Center.
+
+package crc.dps.tagset;
+
+/** The Basic tagset.  This consists of the HTML syntax plus those
+ *	actors that have no effect outside the file being processed.
+ *	It is somewhat inefficient to extend HTML_ts rather than
+ *	including it; we may want to revisit this decision later. */
+public class basic extends HTML_ts {
+
+  static String emptyActors = "actor-attrs actor-dscr actor-doc actor-syntax"
+  + " get tagset-exists tagset-load tagset-include element actor-names"
+  + " calendar";
+  static String parsedActors = "add-markup difference equal expand if"
+  + " pad product protect-result quotient set sort sorted subst sum"
+  + " tagset test text trim user-message calendar-day form";
+  static String quotedActors = "protect repeat foreach actor process";
+
+  public basic() {
+    this("Basic", true);
+  }
+
+  basic(String name, boolean emptyParagraphTags) {
+    super(name, emptyParagraphTags);
+    
+    defActive(emptyActors, null, EMPTY);
+    defActive(parsedActors, null, NORMAL);
+    defActive(quotedActors, null, QUOTED);
+
+    // -foreach- needs a match.
+    //Actor a = new Actor("-foreach-", null, "quoted", "foreach_");
+    //a.attr("match", "foreach");
+    //a.initMatch();
+    //define(a);
+
+    // ... or could do defActors("then else", "quoted", true);
+    defActive("then else", "then else", QUOTED);
+  }
+
+}
+
---
+++ src/java/crc/dps/tagset/standard.java
@@ -0,0 +1,28 @@
+////// standard.java:  Initializer for Standard tagset
+//	$Id$
+//	Copyright 1997, Ricoh California Research Center.
+
+package crc.dps.tagset;
+
+/** The Standard tagset.  This consists of the HTML syntax plus all
+ *	actors that normally used <em>inside</em> the PIA. */
+public class standard extends standalone {
+
+  static String emptyActors = "agent-criteria"
+  + " agent-home agent-list agent-options agent-running agent-remove"
+  + " agent-set-criterion agent-restore agent-save authenticate"
+  + " pia-exit get.agent get.form get.pia get.trans";
+
+  static String parsedActors = "agent-set-criteria agent-set-options"
+  + " agent-install password-file-entry" 
+  + " submit-forms trans-control set.agent set.form set.pia set.trans"; 
+
+  public standard() {
+    super("Standard", true);
+
+    defActive(emptyActors, null, EMPTY);
+    defActive(parsedActors, null, NORMAL);
+  }
+
+}
+
