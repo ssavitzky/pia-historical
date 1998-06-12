d0ed2f9e14f462b044e3960e18b4156de7bac5fd
steve <>
1998-06-12 00:14:07 +0000

    Added Proxy and subclass FilterText; OutputTrace is now subclass of Proxy

---
+++ src/java/crc/dps/output/FilterText.java
@@ -0,0 +1,56 @@
+////// FilterText: text-only filter for an Output
+//	$Id$
+//	Copyright 1998, Ricoh Silicon Valley.
+
+package crc.dps.output;
+
+import crc.dom.*;
+import crc.dps.*;
+import crc.dps.NodeType;
+
+import java.io.PrintStream;
+
+/**
+ * An Output filter that passes only Text nodes <p>
+ *
+ * @version $Id$
+ * @author steve@rsv.ricoh.com 
+ */
+
+public class FilterText extends Proxy {
+
+  /************************************************************************
+  ** Operations:
+  ************************************************************************/
+
+  public void putNode(Node aNode) { 
+    if (target != null && aNode.getNodeType() == NodeType.TEXT)
+      target.putNode(aNode);
+  }
+  public void startNode(Node aNode) { 
+    depth++;
+  }
+  public boolean endNode() { 
+    depth --;
+    return depth >= 0;
+  }
+  public void startElement(Element anElement) {
+    depth++;
+  }
+  public boolean endElement(boolean optional) {
+    depth --;
+    return depth >= 0;
+  }
+
+  /************************************************************************
+  ** Construction:
+  ************************************************************************/
+
+  public FilterText() {
+  }
+
+  public FilterText(Output theTarget) {
+    super(theTarget);
+  }
+
+}
--- src/java/crc/dps/output/Proxy.java
+++ src/java/crc/dps/output/Proxy.java
@@ -1,4 +1,4 @@
-////// OutputTrace: debugging shim for an Output
+////// Proxy: filtering proxy for an Output
 //	$Id$
 //	Copyright 1998, Ricoh Silicon Valley.
 
@@ -11,64 +11,49 @@ import crc.dps.aux.Log;
 import java.io.PrintStream;
 
 /**
- * A debugging shim for Outputs.  All operations are proxied to a
- *	``real'' target Output, and also logged to a PrintStream. <p>
+ * A proxy for DPS Outputs.  All operations are proxied to a
+ *	``real'' target Output. <p>
+ *
+ *	A Proxy can be used with no target to simply discard output.
+ *	This is reasonably efficient.
  *
  * @version $Id$
  * @author steve@rsv.ricoh.com 
  */
 
-public class OutputTrace extends Proxy {
+public class Proxy implements Output {
 
   /************************************************************************
   ** State:
   ************************************************************************/
 
-  protected PrintStream log	= System.err;
-  protected static String NL	= "\n";
+  protected Output target	= null;
+  protected int depth		= 0;
 
-  public void setLog(PrintStream s) { log = s; }
-
-  public void debug(String message) {
-    log.print(message);
-  }
-
-  public void debug(String message, int indent) {
-    String s = "=>";
-    for (int i = 0; i < indent; ++i) s += " ";
-    s += message;
-    log.print(s);
-  }
-
-  public String logNode(Node aNode) { return Log.node(aNode); }
-  public String logString(String s) { return Log.string(s); }
+  public Output getTarget() { return target; }
+  public void setTarget(Output theTarget) { target = theTarget; }
 
   /************************************************************************
   ** Operations:
   ************************************************************************/
 
   public void putNode(Node aNode) { 
-    debug("put " + logNode(aNode) + NL, depth);
     if (target != null) target.putNode(aNode);
   }
   public void startNode(Node aNode) { 
-    debug("start " + logNode(aNode) + NL, depth);
     depth++;
     if (target != null) target.startNode(aNode);
   }
   public boolean endNode() { 
     depth --;
-    debug("end " + NL, depth);
     return (target != null)? target.endNode() : depth >= 0;;
   }
   public void startElement(Element anElement) {
-    debug("start " + logNode(anElement) + NL, depth);
     depth++;
     if (target != null) target.startElement(anElement);
   }
   public boolean endElement(boolean optional) {
     depth --;
-    debug("end(" + (optional? "true" : "false") +")" + NL, depth);
     return (target != null)? target.endElement(optional) : depth >= 0;
   }
 
@@ -76,15 +61,10 @@ public class OutputTrace extends Proxy {
   ** Construction:
   ************************************************************************/
 
-  public OutputTrace() {
+  public Proxy() {
   }
 
-  public OutputTrace(Output theTarget) {
-    super(theTarget);
-  }
-
-  public OutputTrace(Output theTarget, PrintStream theLog) {
-    super(theTarget);
-    log = theLog;
+  public Proxy(Output theTarget) {
+    target = theTarget;
   }
 }
