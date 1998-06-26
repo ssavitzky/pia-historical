485727c8872755ec4b89b5bdddf0eac4ef9499e3
steve <>
1998-06-26 01:56:10 +0000

    Added MIN_TYPE, MAX_TYPE, getName(type)

--- src/java/crc/dps/NodeType.java
+++ src/java/crc/dps/NodeType.java
@@ -16,4 +16,17 @@ public class NodeType extends crc.dom.NodeType {
   public static final int ENDTAG = -1;
   public static final int NODELIST = -2;
   public static final int DECLARATION = -3;
+
+  public static final int MIN_TYPE = -3;
+  public static final int MAX_TYPE = ENTITY;
+
+  public static final String names[] = {
+    "ENDTAG", 	"NODELIST",	"DECLARATION", 
+    "DOCUMENT",	"ELEMENT", 	"ATTRIBUTE",
+    "PI",	"COMMENT",	"TEXT",
+    "ENTITY",};
+
+  public static String getName(int type) {
+    return names[type-MIN_TYPE];
+  }
 }
