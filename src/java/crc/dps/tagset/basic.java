8c4d4c2d7201b2b7d14a1d8ca2f3375f534e41bf
steve <>
1998-07-10 01:11:10 +0000

    added repeatHandler. Cache handlers so that classes are explicitly loaded, and undefined ones don't have to be looked up at run time.

--- src/java/crc/dps/tagset/basic.java
+++ src/java/crc/dps/tagset/basic.java
@@ -4,13 +4,21 @@
 
 package crc.dps.tagset;
 
+import crc.ds.Table;		// for cache
+import crc.dps.handle.*;	// for cache initialization
+
 /** The Basic tagset.  This consists of the HTML syntax plus those
  *	actors that have no effect outside the file being processed.
  *	It is somewhat inefficient to extend HTML_ts rather than
- *	including it; we may want to revisit this decision later. */
+ *	including it; we may want to revisit this decision later. <p>
+ *
+ *	This class includes a static cache for handler instances by class name
+ *	(actually by ``<code>cname</code>'').  This greatly speeds up 
+ *	initialization because all classes are identified at compile time. <p>
+ */
 public class basic extends HTML_ts {
 
-  static String emptyActors = "actor-attrs actor-dscr actor-doc actor-syntax"
+  static String emptyActors = ""
   + " get tagset-exists tagset-load tagset-include element actor-names"
   + " calendar";
   static String parsedActors = "add-markup difference equal expand if"
@@ -37,6 +45,53 @@ public class basic extends HTML_ts {
 
     defActive("then", "test else", QUOTED);
     defActive("else else-if", "test then else-if", QUOTED);
+  }
+
+  /************************************************************************
+  ** Handler cache:
+  ************************************************************************/
+
+  /** Override this to return <code>true</code> in order to make the set of
+   *	handler classes open-ended (i.e. loadable by name at run-time).
+   */
+  protected boolean openEnded() { return false; }
+
+  protected static Table handlerCache = new Table();
+  protected static void defHandle(String cname, BasicHandler handler) {
+    handlerCache.at(cname, handler);
+  }
+
+  static {
+    defHandle("else", new elseHandler());
+    defHandle("elsf", new elsfHandler());
+    defHandle("get", new getHandler());
+    defHandle("if", new ifHandler());
+    defHandle("repeat", new repeatHandler());
+    defHandle("set", new setHandler());
+    defHandle("subst", new substHandler());
+    defHandle("test", new testHandler());
+    defHandle("then", new thenHandler());
+  }
+
+  /** Instantiate an appropriate handler.  Uses a static cache for speed.
+   */
+  protected BasicHandler loadHandler(String cname) {
+    BasicHandler h = (BasicHandler) handlerCache.at(cname);
+    if (h == null) h = openEnded()
+		     ? super.loadHandler(cname)
+		     : new BasicHandler();
+    return h;
+  }
+
+  /** Instantiate an appropriate handler.  Uses a static cache for speed.
+   */
+  protected GenericHandler loadHandler(String tag, String cname) {
+    if (cname == null) cname = tag;
+    GenericHandler h = (GenericHandler) handlerCache.at(cname);
+    if (h == null) h = openEnded()
+		     ? super.loadHandler(tag, cname)
+		     : new GenericHandler();
+    return h;
   }
 
 }
--- src/java/crc/dps/tagset/legacy.java
+++ src/java/crc/dps/tagset/legacy.java
@@ -6,16 +6,22 @@ package crc.dps.tagset;
 
 import crc.util.*;
 import crc.dps.handle.*;
+import crc.interform.handle.*;
 
 /** The Legacy tagset.  This consists of the HTML syntax plus those
- *	actors that were used in old (legacy) InterForms. */
+ *	actors that were used in old (legacy) InterForms. <p>
+ *
+ *	This class includes a static cache for handler instances by class name
+ *	(actually by ``<code>cname</code>'').  This greatly speeds up 
+ *	initialization because all classes are identified at compile time. <p>
+ */
 public class legacy extends HTML_ts {
 
   static String emptyActors = "actor-attrs actor-dscr actor-doc actor-syntax"
   + " get tagset-exists tagset-load tagset-include element actor-names"
   + " calendar"
   /* from standalone */
-  + " file read get.env read.file read.href authenticate"
+  + " file read authenticate"
   /* from standard */
   + " agent-criteria agent-home agent-list agent-options agent-running"
   + " agent-remove agent-set-criterion agent-restore agent-save"
@@ -36,10 +42,127 @@ public class legacy extends HTML_ts {
     this("Legacy", true);
   }
 
+  legacy(String name, boolean emptyParagraphTags) {
+    super(name, emptyParagraphTags);
+    
+    defActive(emptyActors, null, EMPTY);
+    defActive(parsedActors, null, NORMAL);
+    defActive(quotedActors, null, QUOTED);
+
+    // -foreach- needs a match.
+    //Actor a = new Actor("-foreach-", null, "quoted", "foreach_");
+    //a.attr("match", "foreach");
+    //a.initMatch();
+    //define(a);
+
+    // ... or could do defActors("then else", "quoted", true);
+    defActive("then else", "then else", QUOTED);
+  }
+
+  /************************************************************************
+  ** Handler cache:
+  ************************************************************************/
+
+  protected static crc.ds.Table handlerCache = new crc.ds.Table();
+  protected static void defHandle(String cname, BasicHandler handler) {
+    handlerCache.at(cname, handler);
+  }
+
+  protected static void defLegacy(String cname, crc.interform.Handler h) {
+    handlerCache.at(cname, new LegacyHandler(h));
+  }
+
+  static {
+    defHandle("else", new elseHandler());
+    defHandle("elsf", new elsfHandler());
+    defHandle("get", new getHandler());
+    defHandle("if", new ifHandler());
+    defHandle("repeat", new repeatHandler());
+    defHandle("set", new setHandler());
+    defHandle("subst", new substHandler());
+    defHandle("test", new testHandler());
+    defHandle("then", new thenHandler());
+
+    // === need Table, Dl, etc. ===
+
+    defLegacy("actor-attrs", new Actor_attrs());
+    defLegacy("actor-dscr", new Actor_dscr());
+    defLegacy("actor-doc", new Actor_doc());
+    defLegacy("actor-syntax", new Actor_syntax());
+    defLegacy("tagset-exists", new Tagset_exists());
+    defLegacy("tagset-load", new Tagset_load());
+    defLegacy("tagset-include", new Tagset_include());
+    defLegacy("element", new Element());
+    defLegacy("actor-names", new Actor_names());
+    defLegacy("calendar", new Calendar());
+    defLegacy("file", new File());
+    defLegacy("read", new Read());
+    defLegacy("authenticate", new Authenticate());
+    defLegacy("agent-criteria", new Agent_criteria());
+    defLegacy("agent-home", new Agent_home());
+    defLegacy("agent-list", new Agent_list());
+    defLegacy("agent-options", new Agent_options());
+    defLegacy("agent-running", new Agent_running());
+    defLegacy("agent-remove", new Agent_remove());
+    defLegacy("agent-set-criterion", new Agent_set_criterion());
+    defLegacy("agent-restore", new Agent_restore());
+    defLegacy("agent-save", new Agent_save());
+    defLegacy("pia-exit", new Pia_exit());
+    defLegacy("add-markup", new Add_markup());
+    defLegacy("difference", new Difference());
+    defLegacy("equal", new Equal());
+    defLegacy("expand", new Expand());
+    defLegacy("pad", new Pad());
+    defLegacy("product", new Product());
+    defLegacy("protect-result", new Protect_result());
+    defLegacy("quotient", new Quotient());
+    defLegacy("sort", new Sort());
+    defLegacy("sorted", new Sorted());
+    defLegacy("subst", new Subst());
+    defLegacy("sum", new Sum());
+    defLegacy("tagset", new Tagset());
+    defLegacy("text", new Text());
+    defLegacy("trim", new Trim());
+    defLegacy("user-message", new User_message());
+    defLegacy("calendar-day", new Calendar_day());
+    defLegacy("form", new Form());
+    defLegacy("os-command", new Os_command());
+    defLegacy("os-command-output", new Os_command_output());
+    defLegacy("write", new Write());
+    defLegacy("password-file-entry", new Password_file_entry());
+    defLegacy("agent-set-criteria", new Agent_set_criteria());
+    defLegacy("agent-set-options", new Agent_set_options());
+    defLegacy("agent-install", new Agent_install());
+    defLegacy("submit-forms", new Submit_forms());
+    defLegacy("trans-control", new Trans_control());
+    defLegacy("protect", new Protect());
+    defLegacy("foreach", new Foreach());
+    defLegacy("actor", new Actor());
+    defLegacy("process", new crc.interform.handle.Process());
+
+  }
+
+  /** Instantiate an appropriate handler.  Uses a static cache for speed.
+   */
+  protected BasicHandler loadHandler(String cname) {
+    BasicHandler h = (BasicHandler) handlerCache.at(cname);
+    if (h == null) h = new BasicHandler();
+    return h;
+  }
+
+  /** Instantiate an appropriate handler.  Uses a static cache for speed.
+   */
+  protected GenericHandler loadHandler(String tag, String cname) {
+    if (cname == null) cname = tag;
+    GenericHandler h = (GenericHandler) handlerCache.at(cname);
+    if (h == null) h = new GenericHandler();
+    return h;
+  }
+
   /** Load an appropriate handler class and instantiate it. 
    *	Subclasses (e.g. legacyTagset) may need to override this.
    */
-  protected GenericHandler loadHandler(String tag, String cname) {
+  protected GenericHandler old_loadHandler(String tag, String cname) {
     GenericHandler h = null;
     String name = (cname == null)? tag : cname;
     // First shoot for a non-legacy handler that does the same thing.
@@ -59,23 +182,6 @@ public class legacy extends HTML_ts {
     } catch (Exception e) {}
     if (h == null) h = new GenericHandler();
     return h;
-  }
-
-  legacy(String name, boolean emptyParagraphTags) {
-    super(name, emptyParagraphTags);
-    
-    defActive(emptyActors, null, EMPTY);
-    defActive(parsedActors, null, NORMAL);
-    defActive(quotedActors, null, QUOTED);
-
-    // -foreach- needs a match.
-    //Actor a = new Actor("-foreach-", null, "quoted", "foreach_");
-    //a.attr("match", "foreach");
-    //a.initMatch();
-    //define(a);
-
-    // ... or could do defActors("then else", "quoted", true);
-    defActive("then else", "then else", QUOTED);
   }
 
 }
