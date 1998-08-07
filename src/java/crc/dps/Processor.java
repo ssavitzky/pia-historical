423c916b5e6af44ebd4c1745b610dd704b1232d8
steve <>
1998-08-07 01:37:54 +0000

    Top-level documentation comment fixed.  Lot of old stuff removed.

--- src/java/crc/dps/Processor.java
+++ src/java/crc/dps/Processor.java
@@ -8,48 +8,23 @@ import crc.dom.NodeList;
 
 /**
  * The interface for a document Processor. 
- *	A Processor maintains two stacks: 
- *	<ol><li> an `input stack' of Input objects that are a source of Token
- *		 objects to be processed.
- *	    <li> a `parse stack' consisting of a parse tree under construction
- *		 along with additional processor state.
- *	</ol>
  *
- *	There are three ways in which an object can interface to a 
- *	Processor:
+ *  <p>	A Processor has an Input, an Output, and a Context; the latter is a
+ *	dynamic link up the processing context stack.  Processor
+ *	<em>extends</em> Context, which means that it also has an entity
+ *	table.
  *
- *	<ol>
- *	    <li> ``Pull mode'' -- the object requests each individual
- *		 Token.  This treats the Processor as an Input.  Nothing
- *		 special has to be done in this case.
- *
- *	    <li> ``Push mode'' -- an Output registers itself with the
- *		 Processor, which then feeds (pushes) Token objects
- *		 to the Output as they become available.  In this case
- *		 the Processor runs ``to completion,'' which is somewhat
- *		 more efficient.
- *
- *	    <li> ``Parse mode'' -- the Processor constructs a complete
- *		 Document, which the object requests.
- *	</ol>
- *
- *	A Processor will normally ensure that any element started inside
- *	an Input is ended when that Input is popped off the stack.<p>
- *
- * === The ideal thing would be for a Processor to build documents
- *	using Token only for the nodes that will have to be
- *	executable, and objects out of crc.dom for everything else.
- *	Tricky.  Wonder whether the different ``ways'' above correspond
- *	to different extensions of Processor...
- *
- * === NOTE: Both Parser and Processor need DTD and parse stack info. ===
- * === it's up to the Parser to associate Handler, etc. with Token. ===
+ *  <p>	A TopContext (q.v.) is a specialized Processor with links to its
+ *	current Tagset and, possibly, Parser.
  *
  * @version $Id$
  * @author steve@rsv.ricoh.com
  *
- * @see crc.dps.Token
- * @see crc.dps.Input */
+ * @see crc.dps.Context
+ * @see crc.dps.Input 
+ * @see crc.dps.Output
+ * @see crc.dps.TopContext
+ */
 
 public interface Processor extends Context {
 
