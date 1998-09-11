e325d21673401e6b7d0df43a0a9718ba575bdfc3
wolff <>
1998-09-11 08:43:47 +0000

    *** empty log message ***

---
+++ Config/Printer/my/Printer/styles/contract.ts
@@ -0,0 +1,20 @@
+
+     
+ 
+     
+ 
+      
+ <tagset name=Printer-styles-contract base=Standard>
+ <!-- ignored buggy--> </tagset>
+ <tagset-load name=Printer-styles-contract>
+
+<actor tag=short-quote>
+<!-- ignore -->
+</actor> 
+ 
+    
+ 
+ 
+     
+ 
+    
---
+++ Config/Printer/my/Printer/styles/makebook.ts
@@ -0,0 +1,15 @@
+
+    <tagset name=Printer-styles-makebook base=Standard> </tagset>
+ <tagset-load name=Printer-styles-makebook>
+ 
+<!-- ignore form tags, input, etc. -->
+
+<actor tag=form>
+&content;
+</actor>
+<actor tag=input empty>
+</actor>
+<actor tag=textarea>
+</actor>
+
+    
