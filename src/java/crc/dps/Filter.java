6391e1f6a343be04be71b377f67fe109b5c71cb9
steve <>
1998-10-02 23:12:22 +0000

    Default tagset now "standard"

--- src/java/crc/dps/Filter.java
+++ src/java/crc/dps/Filter.java
@@ -36,7 +36,7 @@ public class Filter {
   static String infile = null;
   static String outfile = null;
   static String propfile = null;
-  static String tsname = "Basic";
+  static String tsname = "standard";
   static boolean tree = false;
   static boolean entities = true;
   static boolean parsing = false;
