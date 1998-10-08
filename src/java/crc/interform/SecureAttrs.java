1fb6ffe7a9849d95004faaa9f91343cbe1a6ca70
pgage <>
1998-10-08 18:07:54 +0000

    Removed attrs methods, added extends AttrTabular.

--- src/java/crc/interform/SecureAttrs.java
+++ src/java/crc/interform/SecureAttrs.java
@@ -11,9 +11,10 @@ import crc.interform.Util;
 import crc.sgml.SGML;
 import crc.sgml.Attrs;
 import crc.sgml.AttrTable;
-import crc.sgml.AttrWrap;
+import crc.sgml.AttrTabular;
 
 import crc.ds.List;
+import crc.ds.Tabular;
 
 /** A ``secure'' SGML wrapper for an Attrs object.  It is secure in
  *	the sense that it can check the Interp and its Environment to
@@ -21,45 +22,10 @@ import crc.ds.List;
  *	context.  Since it extends <code>crc.sgml.AttrSGML</code>, it
  *	can be put into an <code>attrs</code> object. 
  */
-public class SecureAttrs extends AttrWrap {
+public class SecureAttrs extends AttrTabular {
 
   protected Interp context;
 
-  /************************************************************************
-  ** Attrs interface:  Abstract:
-  ************************************************************************/
-
-  /** Return the number of defined attributes. */
-  public int nAttrs() {
-    // === security checks unimplemented ===
-    return (attributes == null)? 0 : attributes.nAttrs();
-  }
-
-  /** Test whether an attribute exists. */
-  public boolean hasAttr(String name) {
-    // === security checks unimplemented ===
-    return (attributes == null)? false : attributes.hasAttr(name);
-  }
-  
-  /** Retrieve an attribute by name.  Returns null if no such
-   *	attribute exists. */
-  public SGML attr(String name) {
-    // === security checks unimplemented ===
-    return (attributes == null)? null : attributes.attr(name);
-  }
-
-  /** Enumerate the defined attributes. */
-  public java.util.Enumeration attrs() {
-    // === security checks unimplemented ===
-    return (attributes == null)? new AttrTable().attrs() : attributes.attrs();
-  }
-
-  /** Set an attribute. */
-  public void attr(String name, SGML value) {
-    // === security checks unimplemented ===
-    if (attributes == null) attributes = new AttrTable();
-    attributes.attr(name, value);
-  }    
 
   /************************************************************************
   ** Construction:
@@ -70,9 +36,9 @@ public class SecureAttrs extends AttrWrap {
     this.context = context;
   }
 
-  /** Make an SecureAttrs out of an Attrs by wrapping it.  The Attrs is
+  /** Make an SecureAttrs using Tabular.  This is
    *	<em>not</em> cloned or copied. */
-  public SecureAttrs(Attrs t, Interp context) {
+  public SecureAttrs(Tabular t, Interp context) {
     super(t);
     this.context = context;
   }
