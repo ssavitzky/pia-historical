2b7f7e0d18ebf8b7140bb10f61669c3ffe975f45
pgage <>
1999-01-22 17:58:15 +0000

    Tags for group_edit page mostly done.

--- Agents/RHServer/RHServer-xhtml.ts
+++ Agents/RHServer/RHServer-xhtml.ts
@@ -1,8 +1,205 @@
 <!doctype tagset system "tagset.dtd">
 <tagset name=RHServer-xhtml parent=pia-xhtml recursive>
 
-<h1>Simple-xhtml Tagset</h1>
+<h1>RHServer Tagset</h1>
 
+<h2>Data Extraction Tags</h2>
+
+
+<define element=get-all-concepts>
+	<doc>
+		Retrieves all nodes of type rhconcept and lists
+		them by name; i.e. returns their name attribute.
+	</doc>
+	<action>
+		<select><from>&AGENT:all-concepts;</from><name recursive>rhconcept</name>
+			<repeat list=&selected;>
+				<OPTION><select><from>&li;</from><name recursive>rhconcept-name</name><content>
+				</select>
+			</repeat>
+		</select>
+	</action>
+</define>
+
+<define element=get-concept-by-name>
+	<doc> Given a concept name, returns the concept.
+	</doc>
+	<define attribute=name required></define>
+	<action>
+		<select><from>&AGENT:all-concepts;</from><name recursive>rhconcept</name>
+			<repeat> <foreach entity=x>&selected;></foreach>
+				<select><from>&x;</from><name recursive>rhconcept-name</name><content>
+					<if><test match=&attributes:name;>&selected;</test><then>
+						&x;
+					</if>
+				</select>
+			</repeat>
+	</action>
+</define>
+
+<define element=get-concept-field>
+	<doc>Given a concept name and a concept field name,
+		returns the value for that field.
+	</doc>
+	<define attribute=name required></define>
+	<define attribute=field required></define>
+	<action>
+		<set name=cpt>
+			<get-concept-by-name name=&attributes:name;>
+			</get-concept-by-name>
+		</set>
+		<select><from>&cpt;</from><name recursive>&attributes:field;</name><content>
+		</select>
+	</action>
+</define>
+
+
+<define element=get-all-phrases>
+	<doc> Given a concept name, retrieves all phrases
+		associated with that concept.
+	</doc>
+	<define attribute=concept required></define>
+	<action>
+		<set name=cpt>
+			<get-concept-by-name name=&attributes:concept;>
+			</get-concept-by-name>
+		</set>
+		<select><from>&cpt;</from><name recursive>rhconcept-phraselist</name>
+			<repeat><foreach entity=x> &selected;></foreach>
+				<select><from>&x;</from><name recursive>rhconcept-phrase</name><content>
+				</select>
+			</repeat>
+	</action>
+</define>
+
+<define element=remove-concept-group>
+	<doc>Given a concept name, and a group name,
+		 removes this group from the concept
+	</doc>
+	<define attribute=concept required></define>
+	<define attribute=group required></define>
+	<action>
+		<set name=cpt>
+			<get-concept-by-name name=&attributes:concept;>
+			</get-concept-by-name>
+		</set>
+		<select><from>&cpt;</from><name recursive>rhconcept-group</name>
+			<repeat><foreach entity=x>&selected;</foreach>
+				<set name=gname><select>&x;<content></select></set>
+				<if><test match=&attributes:group;>&gname;</test><then>
+					<select>&x;
+						<remove>&selected;</remove>
+					</select>
+			
+				</if>
+			</repeat>
+		</select>
+	</action>
+</define>
+
+
+<define element=get-all-concept-groups>
+	<doc> Given a concept name, retrieves all group names
+		associated with that concept.
+	</doc>
+	<define attribute=concept required></define>
+	<action>
+		<set name=cpt>
+			<get-concept-by-name name=&attributes:concept;>
+			</get-concept-by-name>
+		</set>
+		<select><from>&cpt;</from><name recursive>rhconcept-grouplist</name>
+			<repeat><foreach entity=x> &selected;></foreach>
+				<select><from>&x;</from><name recursive>rhconcept-group</name><content>
+				</select>
+			</repeat>
+	</action>
+</define>
+
+<define element=get-all-groups>
+	<doc> Extracts all group strings from the rhconcept-group fields
+		in the piaconcept data structure.  Returns each group in
+		a menu op;tion format.
+	</doc>
+	<action>
+	<select><from>&AGENT:all-concepts;</from><name recursive>rhconcept-group</name><content>
+		<unique>&selected;</unique>
+		<repeat><foreach entity=x>&selected;</foreach>
+			<OPTION>&x;
+		</repeat>
+	</select>
+	</action>
+</define>
+
+
+<define element=get-group-by-name>
+	<doc>Given a group name, returns that group. This may fail
+		 to match where a group-list contains several items.
+	</doc>
+	<define attribute=group required></define>
+	<action>
+		<select><from>&AGENT:all-concepts;</from><name recursive>rhconcept-group</name><content>
+			<repeat><foreach entity=x>&selected;</foreach>
+				<if><test match=&attributes:group;>&x;</test><then>
+					&x;
+				</if>
+			</repeat>
+		</select>
+
+	</action>
+</define>
+
+
+<define element=remove-group>
+	<doc>Given a group name, removes this group from
+		all concepts that list this group as a
+		member.
+	</doc>
+	<define attribute=group required></define>
+	<action>
+		<select><from>&AGENT:all-concepts;</from><name recursive>rhconcept-group</name>
+			<repeat><foreach entity=x>&selected;</foreach>
+				<set name=gname><select>&x;<content></select></set>
+				<if><test match=&attributes:group;>&gname;</test><then>
+					<select>&x;
+						<remove>&selected;</remove>
+					</select>
+				</if>
+			</repeat>
+		</select>
+	</action>
+</define>
+
+<define element=get-all-group-concepts>
+	<doc> Given a group name, retrieves all concepts
+		associated with that group.  Returns the
+		concept name.
+	</doc>
+	<define attribute=group required></define>
+	<action>
+		<set name=cpt>
+			<select><from>&AGENT:all-concepts;</from><name recursive>rhconcept</name>
+			</select>
+		</set>
+		<select>
+			<repeat><foreach entity=x>&cpt;</foreach>
+				<!-- Find group by name within individual concept x -->
+				<select><from>&x;</from><name recursive>rhconcept-group</name><content>
+					<repeat><foreach entity=y>&selected;</foreach>
+						<!-- If group matches name, return the concept name -->
+						<if><test match=&y;>&attributes:group;</test><then>
+							<OPTION><select><from>&x;</from><name recursive>rhconcept-name</name><content>
+							</select>
+						</if>
+					</repeat>
+				</select>
+			</repeat>
+		</select>
+	</action>
+</define>
+
+
+<!-- Probably obsolete -->
 <define element=extract-content>
 	<doc> Extracts a phrase from a node.  The parent node is
 		of type rhconcept-phrase.  This tag first calculates
@@ -26,15 +223,14 @@
 	</define>
 </define>
 
-
+<!-- Probably obsolete -->
 <define element=concepts>
 	<doc> Extracts all concept strings from the rhconcept-phrase fields
 		in the piaconcept data structure.
 	</doc>
 	<define attribute=path required></define>
 	<action>
-	<set name=consrc><connect src=&attributes:path; entity=rhcon></connect></set>
-	<select><from>&rhcon;</from><name recursive>rhconcept-phrase</name>
+	<select><from>&AGENT:all-concepts;</from><name recursive>rhconcept-phrase</name>
 		<repeat list=&selected;>
 			<OPTION><text split sep=" "><extract-content entity=&li;></extract-content></text>
 		</repeat>
@@ -43,6 +239,7 @@
 	</action>
 </define>
 
+<h2>Layout and Appearance Tags</h2>
 
 <define entity=blue-dot>
   <value><img src="/Agency/Icons/dot-blue.gif"
@@ -78,12 +275,66 @@
     <td>
     <table cellspacing=0 cellpadding=0 border=0>
     <tr><th align=left nowrap width=170>&blank-170x1;<td><br>
-    <tr><th align=right><xopt page="&attributes:page;"
+	<if><test match=&attributes:page;>home</test<then>
+	    <tr><th align=right><xopt page="&attributes:page;"
 			      pages="home profiles groups concepts">&blue-dot;</xopt>
-	<td> <xa href="home" page="&attributes:page;">Home</xa>
-    	     <xa href="profiles" page="&attributes:page;">Profiles</xa>
-    	     <xa href="groups" page="&attributes:page;">Groups</xa>
-	     <xa href="concepts" page="&attributes:page;">Concepts</xa>
+		<td> <xa href="home" page="&attributes:page;">Home</xa><!--Home Page-->
+    	     	<xa href="profiles" page="&attributes:page;">Profiles</xa>
+    	     	<xa href="groups" page="&attributes:page;">Groups</xa>
+	     	<xa href="concepts" page="&attributes:page;">Concepts</xa>
+		</td>
+	    </tr>
+	</then>
+	</if>
+	<if><test match=&attributes:page;>profiles</test><then>
+	    <tr><th align=right><xopt page="&attributes:page;"
+			      pages="profiles home">&blue-dot;</xopt>
+		<td> <xa href="profiles" page="&attributes:page;">Profiles</xa><!--Profiles page-->
+	     	<xa href="home" page="&attributes:page;">Home</xa>
+		</td>
+	    </tr>	
+	</then>
+	</if>
+	<if><test match=&attributes:page;>groups</test><then>
+	    <tr><th align=right><xopt page="&attributes:page;"
+			      pages="groups profiles home">&blue-dot;</xopt>
+		<td> <xa href="groups" page="&attributes:page;">Groups</xa><!--Groups page-->
+	     	     <xa href="home" page="&attributes:page;">Home</xa>
+                     <xa href="profiles" page="&attributes:page;">Profiles</xa>
+		</td>
+	    </tr>
+	</then>
+	</if>
+	<if><test match=&attributes:page;>concepts</test><then>
+	    <tr><th align=right><xopt page="&attributes:page;"
+			      pages="concepts profiles home">&blue-dot;</xopt>
+	    <td> <xa href="concepts" page="&attributes:page;">Concepts</xa><!--Concepts page-->
+             <xa href="home" page="&attributes:page;">Home</xa>
+             <xa href="profiles" page="&attributes:page;">Profiles</xa>
+	    </td>
+            </tr>	
+	</then>
+	</if>
+	<if><test match=&attributes:page;>group_edit</test><then>
+	    <tr><th align=right><xopt page="&attributes:page;"
+			      pages="group_edit profiles home">&blue-dot;</xopt>
+		<td> <xa href="group_edit" page="&attributes:page;">Group_Edit</xa><!--Group_Edit page-->
+        	     <xa href="groups" page="&attributes:page;">Groups</xa>
+	     	     <xa href="home" page="&attributes:page;">Home</xa>
+		</td>
+	    </tr>	
+	</then>
+	</if>
+	<if><test match=&attributes:page;>concept_edit</test><then>
+   	 <tr><th align=right><xopt page="&attributes:page;"
+			      pages="concept_edit profiles home">&blue-dot;</xopt>
+		<td> <xa href="concept_edit" page="&attributes:page;">Concept_Edit</xa><!--Concept_Edit page-->
+           	  <xa href="concepts" page="&attributes:page;">Concepts</xa>
+	    	 <xa href="home" page="&attributes:page;">Home</xa>
+		</td>
+    	</tr>
+	</then>
+	</if>	
    <get name=content>
   </table>
 </table>
