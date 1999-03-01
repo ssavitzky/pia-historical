fa1c01a8b6ebd4c768625b3f140ea538b5d7f5fb
pgage <>
1999-03-01 23:48:29 +0000

    Added copyright notice.

--- src/java/crc/pia/Logger.java
+++ src/java/crc/pia/Logger.java
@@ -1,6 +1,26 @@
 // CommonLogger.java
 // Id: CommonLogger.java,v 1.12 1996/10/01 18:45:00 abaird Exp 
-// (c) COPYRIGHT MIT and INRIA, 1996.
+
+/*****************************************************************************
+ * The contents of this file are subject to the Ricoh Source Code Public
+ * License Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.risource.org/RPL
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * This code was initially developed by Ricoh Silicon Valley, Inc.  Portions
+ * created by Ricoh Silicon Valley, Inc. are Copyright (C) 1995-1999.  All
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ ***************************************************************************** 
+*/
+
 // Please first read the full copyright statement in file COPYRIGHT.html
 //
 // Logger.java -- modified from CommonLogger; does not inherit from any class
--- src/java/crc/pia/Piaproperties.java
+++ src/java/crc/pia/Piaproperties.java
@@ -1,6 +1,26 @@
 // ObservableProperties.java
 // $Id$
-// (c) COPYRIGHT MIT and INRIA, 1996.
+
+/*****************************************************************************
+ * The contents of this file are subject to the Ricoh Source Code Public
+ * License Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.risource.org/RPL
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * This code was initially developed by Ricoh Silicon Valley, Inc.  Portions
+ * created by Ricoh Silicon Valley, Inc. are Copyright (C) 1995-1999.  All
+ * Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ ***************************************************************************** 
+*/
+
 // Please first read the full copyright statement in file COPYRIGHT.html
 
 package crc.pia;
