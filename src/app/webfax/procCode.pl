74f4a95346876d5d80febfdf926f5061286396d7
pgage <>
1999-03-02 01:02:37 +0000

    Added a copyright notice.

--- src/app/webfax/procCode.pl
+++ src/app/webfax/procCode.pl
@@ -1,4 +1,24 @@
 #!/usr/local/bin/perl
+
+##############################################################################
+ # The contents of this file are subject to the Ricoh Source Code Public
+ # License Version 1.0 (the "License"); you may not use this file except in
+ # compliance with the License.  You may obtain a copy of the License at
+ # http://www.risource.org/RPL
+ #
+ # Software distributed under the License is distributed on an "AS IS" basis,
+ # WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the License
+ # for the specific language governing rights and limitations under the
+ # License.
+ #
+ # This code was initially developed by Ricoh Silicon Valley, Inc.  Portions
+ # created by Ricoh Silicon Valley, Inc. are Copyright (C) 1995-1999.  All
+ # Rights Reserved.
+ #
+ # Contributor(s):
+ #
+############################################################################## 
+
 #Cleanup output from the decode program
 #only return one instance of each decoded thing
 $docDir = "./";			# This is where we should look for doc ids to match the ones found in the code
